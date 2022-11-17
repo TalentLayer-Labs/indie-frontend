9d5973ad9cd6be554abe0d69e197f3d84d178784
akugone <dev@akumaw.com>
2022-11-17 19:52:07 +0100

    PR Fix

--- src/components/Form/ReleaseForm.tsx
+++ src/components/Form/ReleaseForm.tsx
@@ -1,7 +1,8 @@
 import { BigNumber, Signer } from 'ethers';
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { releasePayment } from '../../contracts/releasePayment';
 import { renderTokenAmount } from '../../utils/conversion';
+import { useProvider, useSigner } from '@web3modal/react';
 import { IService, ServiceStatusEnum } from '../../types';
 import { Field, Form, Formik } from 'formik';
 import { config } from '../../config';
@@ -11,30 +12,30 @@ interface IFormValues {
 }
 
 interface IReleaseFormProps {
-  min: number;
-  max: number;
   totalInEscrow: BigNumber;
   rateToken: string;
-  signer: Signer | undefined;
-  provider: any;
   service: IService;
   isBuyer: boolean;
   closeModal: () => void;
 }
 
 function ReleaseForm({
-  min,
-  max,
   totalInEscrow,
   rateToken,
-  signer,
-  provider,
   service,
   closeModal,
   isBuyer,
 }: IReleaseFormProps) {
+  const { data: signer, refetch: refetchSigner } = useSigner();
+  const { provider } = useProvider();
   const [pourcent, setPourcentage] = useState(0);
   const symbol = config.tokens[rateToken].symbol;
+
+  useEffect(() => {
+    (async () => {
+      await refetchSigner({ chainId: 5 });
+    })();
+  }, []);
 
   const handleSubmit = async (values: any) => {
     if (!signer || !provider) {
@@ -47,11 +48,11 @@ function ReleaseForm({
   };
 
   const releaseMax = () => {
-    setPourcentage(max);
+    setPourcentage(100);
   };
 
   const releaseMin = () => {
-    setPourcentage(min);
+    setPourcentage(1);
   };
 
   const onChange = (e: any) => {
@@ -68,79 +69,77 @@ function ReleaseForm({
   const initialValues: IFormValues = {
     pourcentField: '50',
   };
-  if (closeModal) {
-    return (
-      <div className='p-6 space-y-6'>
-        <div className='flex flex-col px-4 py-6 md:p-6 xl:p-6 w-full bg-gray-50 space-y-6'>
-          {service.status === ServiceStatusEnum.Confirmed && (
-            <h3 className='text-xl font-semibold leading-5 text-gray-800'>
-              Select the pourcent to release
-            </h3>
-          )}
-          <div className='flex space-x-2 flex-row'>
-            <div className='items-center rounded-b border-gray-200 '>
-              <button
-                type='button'
-                onClick={releaseMin}
-                className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 '>
-                Min
-              </button>
-            </div>
-            <div className='items-center  rounded-b border-gray-200 '>
-              <button
-                type='button'
-                onClick={releaseMax}
-                className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 '>
-                Max
-              </button>
-            </div>
+
+  return (
+    <div className='p-6 space-y-6'>
+      <div className='flex flex-col px-4 py-6 md:p-6 xl:p-6 w-full bg-gray-50 space-y-6'>
+        {service.status === ServiceStatusEnum.Confirmed && (
+          <h3 className='text-xl font-semibold leading-5 text-gray-800'>
+            Select the pourcent to release
+          </h3>
+        )}
+        <div className='flex space-x-2 flex-row'>
+          <div className='items-center rounded-b border-gray-200 '>
+            <button
+              type='button'
+              onClick={releaseMin}
+              className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 '>
+              Min
+            </button>
           </div>
-          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            <Form>
-              <div className='sm:px-6 justify-between bg-white flex flex-row items-center gap-2'>
-                <div>
-                  <span className='text-base font-semibold leading-4 text-gray-800'>% </span>
-                  <Field
-                    type='number'
-                    label='Pourcent'
-                    className='text-gray-500 py-2 focus:outline-none text-sm sm:text-lg border-0'
-                    placeholder='between 0 and 100'
-                    id='pourcentField'
-                    name='pourcentField'
-                    required
-                    value={pourcent ? pourcent : ''}
-                    onChange={onChange}
-                  />
-                </div>
-                {
-                  <div className='pr-2 text-base font-semibold leading-4 text-gray-400  '>
-                    {amountSelected && renderTokenAmount(rateToken, amountSelected.toString())}
-                    {!amountSelected && '0 ' + `${symbol}`}
-                  </div>
-                }
-              </div>
-              <div className='flex items-center pt-6 space-x-2 rounded-b border-gray-200 '>
-                {isBuyer && totalInEscrow.gt(0) && (
-                  <button
-                    type='submit'
-                    className='text-green-600 bg-green-50 hover:bg-green-500 hover:text-white px-5 py-2 rounded-lg'>
-                    Release the selected amount
-                  </button>
-                )}
-                <button
-                  onClick={closeModal}
-                  type='button'
-                  className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 '>
-                  Close
-                </button>
-              </div>
-            </Form>
-          </Formik>
+          <div className='items-center  rounded-b border-gray-200 '>
+            <button
+              type='button'
+              onClick={releaseMax}
+              className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 '>
+              Max
+            </button>
+          </div>
         </div>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          <Form>
+            <div className='sm:px-6 justify-between bg-white flex flex-row items-center gap-2'>
+              <div>
+                <span className='text-base font-semibold leading-4 text-gray-800'>% </span>
+                <Field
+                  type='number'
+                  label='Pourcent'
+                  className='text-gray-500 py-2 focus:outline-none text-sm sm:text-lg border-0'
+                  placeholder='between 0 and 100'
+                  id='pourcentField'
+                  name='pourcentField'
+                  required
+                  value={pourcent ? pourcent : ''}
+                  onChange={onChange}
+                />
+              </div>
+              {
+                <div className='pr-2 text-base font-semibold leading-4 text-gray-400  '>
+                  {amountSelected && renderTokenAmount(rateToken, amountSelected.toString())}
+                  {!amountSelected && '0 ' + `${symbol}`}
+                </div>
+              }
+            </div>
+            <div className='flex items-center pt-6 space-x-2 rounded-b border-gray-200 '>
+              {isBuyer && totalInEscrow.gt(0) && (
+                <button
+                  type='submit'
+                  className='text-green-600 bg-green-50 hover:bg-green-500 hover:text-white px-5 py-2 rounded-lg'>
+                  Release the selected amount
+                </button>
+              )}
+              <button
+                onClick={closeModal}
+                type='button'
+                className='text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 '>
+                Close
+              </button>
+            </div>
+          </Form>
+        </Formik>
       </div>
-    );
-  }
-  return null;
+    </div>
+  );
 }
 
 export default ReleaseForm;
