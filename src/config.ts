2c1639b69044ffb13062b1f08735ddf3cfc80acf
Romain <romain@talentlayer.org>
2022-11-21 10:20:51 +0100

    use new contracts

--- src/config.ts
+++ src/config.ts
@@ -17,10 +17,10 @@ export const FEE_RATE_DIVIDER = 10_000;
 const goerli: Config = {
   networkId: NetworkEnum.GOERLI,
   contracts: {
-    talentLayerId: '0x20a9A5e013376173C220e9c7AcC8e55137b6C5b1',
-    serviceRegistry: '0xfc2560b8DbC0c52537fEC0399B12B0cd343e71bd',
-    talentLayerReview: '0xE778f68e279D7b618061E9b0387ab6e87F5AE810',
-    talentLayerEscrow: '0xfD06e6F16017Cd32D6E6610B9477169f38B54583',
+    talentLayerId: '0x2F9EBAc36cc020B81c4c624AbcfeC7DC8958Cf54',
+    serviceRegistry: '0x8E86d82214c391281a908e3e31a766fcb9FaB3cA',
+    talentLayerReview: '0x66676968c176aA328124f04f53F8653db6b35ff9',
+    talentLayerEscrow: '0x182980458E0F93167Fab56c494776b694A07C030',
   },
   escrowConfig: {
     timeoutPayment: 3600 * 24 * 7,
@@ -52,10 +52,10 @@ const goerli: Config = {
 const local = {
   networkId: NetworkEnum.LOCAL,
   contracts: {
-    talentLayerId: '0x97aa4622Aeda18CAF5c797C1E5285Bd5c6fc145D',
-    serviceRegistry: '0xE5054E2e59B284CA09713418451709E0CEb4116b',
-    talentLayerReview: '0x67EE2a1f75788794f516b8F9919496D63109A380',
-    talentLayerEscrow: '0xfD06e6F16017Cd32D6E6610B9477169f38B54583',
+    talentLayerId: '0x2F9EBAc36cc020B81c4c624AbcfeC7DC8958Cf54',
+    serviceRegistry: '0x8E86d82214c391281a908e3e31a766fcb9FaB3cA',
+    talentLayerReview: '0x66676968c176aA328124f04f53F8653db6b35ff9',
+    talentLayerEscrow: '0x182980458E0F93167Fab56c494776b694A07C030',
   },
   escrowConfig: {
     timeoutPayment: 3600 * 24 * 7,
