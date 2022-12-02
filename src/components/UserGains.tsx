fcabaea2a57b9fbe65578ae8da3483efa59a7288
Dercio <contact@creedscode.cc>
2022-12-02 09:22:01 +0100

    refactor: :sparkles: Change display of total gain

--- src/components/UserGains.tsx
+++ src/components/UserGains.tsx
@@ -16,29 +16,36 @@ function UserGains({ user }: IProps) {
   return (
     <>
       <h2 className='mb-6 pb-4 border-b border-gray-gray-200 text-gray-900 font-medium'>
-        Your gain history
+        Your total gain
       </h2>
-      <div className='grid grid-cols'>
-        <table className='text-left w-full'>
-          <thead className='flex w-full'>
-            <tr className='flex w-full mb-4'>
-              <th className='p-4 w-1/4'>Gain</th>
-              <th className='p-4 w-1/4'>Token</th>
-            </tr>
-          </thead>
-          <tbody className='flex flex-col items-center justify-between overflow-y-scroll w-full'>
-            {userGains.map((gain, i) => {
-              return (
-                <tr key={i} className='flex w-full mb-4'>
-                  <td className='p-4 w-1/4'>
-                    {renderTokenAmount(gain.token.address, gain.totalGain)}
-                  </td>
-                  <td className='p-4 w-1/4'>{gain.token.name}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+        {userGains.map((gain, i) => {
+          return (
+            <div
+              className='flex items-center p-4 bg-white rounded-xl border border-gray-200'
+              key={i}>
+              <div className='flex flex-shrink-0 items-center justify-center bg-green-200 h-16 w-16 rounded'>
+                <svg
+                  className='w-6 h-6 fill-current text-green-700'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'>
+                  <path
+                    fillRule='evenodd'
+                    d='M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </div>
+              <div className='flex-grow flex flex-col ml-4'>
+                <span className='text-xl font-bold'>
+                  {/* {renderTokenAmount(gain.token.address, gain.totalGain)} */}
+                  {gain.totalGain} {gain.token.name}
+                </span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
