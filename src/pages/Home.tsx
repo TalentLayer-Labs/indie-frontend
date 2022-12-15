769bb70e39bb7e6a24ea8b3d960a456f7690f34e
Bertrand Rigal <bertrandrigal.pro@gmail.com>
2022-12-15 16:55:50 +0100

    💬 Update menu, home page and add archive folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
 import CreateId from '../components/Home/CreateId';
-import SearchService from '../components/Home/SearchService';
-import SearchTalent from '../components/Home/SearchTalent';
 
 function Home() {
   return (
     <>
       <CreateId />
-      <SearchService />
-      <SearchTalent />
     </>
   );
 }
