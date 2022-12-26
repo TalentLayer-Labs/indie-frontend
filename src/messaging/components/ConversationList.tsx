f46cba960d830142d7a50827924a65c48895ac63
Quentin D.C <quent043@hotmail.com>
2022-12-26 10:35:12 +0000

    Functional conversation List

--- src/messaging/components/ConversationList.tsx
+++ src/messaging/components/ConversationList.tsx
@@ -1,23 +1,25 @@
 import { getLatestMessage } from '../utils/messaging';
 import ConversationCard from './ConversationCard';
+import { Message } from '@pushprotocol/restapi/src/lib/chat/ipfs';
 import { DecodedMessage } from '@xmtp/xmtp-js';
+import { pCAIP10ToWallet } from '@pushprotocol/restapi/src/lib/helpers';
 
 interface IConversationListProps {
-  conversationMessages: Map<string, DecodedMessage[]>;
+  conversations: Message[];
   peerAddress: string;
   selectedConversationPeerAddress: string;
 }
 
 const ConversationList = ({
-  conversationMessages,
+  conversations,
   peerAddress,
   selectedConversationPeerAddress,
 }: IConversationListProps) => {
-  const sortedConversations: Map<string, DecodedMessage[]> | undefined = new Map(
-    [...conversationMessages.entries()].sort((convA, convB) => {
-      return getLatestMessage(convA[1])?.sent < getLatestMessage(convB[1])?.sent ? 1 : -1;
-    }),
-  );
+  // const sortedConversations: Map<string, DecodedMessage[]> | undefined = new Map(
+  //   [...conversationMessages.entries()].sort((convA, convB) => {
+  //     return getLatestMessage(convA[1])?.sent < getLatestMessage(convB[1])?.sent ? 1 : -1;
+  //   }),
+  // );
   // const isConvSelected = ethers.utils.getAddress(peerAddress) === selectedConversationPeerAddress;
   // console.log('isConvSelected: ', isConvSelected);
   // console.log('peerAddress: ', peerAddress);
@@ -25,14 +27,14 @@ const ConversationList = ({
 
   return (
     <>
-      {Array.from(sortedConversations.keys()).map(peerAddress => {
+      {conversations.map(message => {
         // if (sortedConversations.get(peerAddress).length > 0) {
         return (
           <ConversationCard
-            key={peerAddress}
+            key={message.toCAIP10}
             // isConvSelected={isConvSelected}
-            peerAddress={peerAddress}
-            latestMessage={getLatestMessage(sortedConversations.get(peerAddress))}
+            peerAddress={pCAIP10ToWallet(message.toCAIP10)}
+            latestMessage={message.messageContent}
           />
         );
       })}
