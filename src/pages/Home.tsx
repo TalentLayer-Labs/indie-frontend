4dd65d43583009d8d4b55dc4a2d936af0815c03f
Bertrand <bertrandrigal.pro@gmail.com>
2022-12-29 11:22:36 +0100

    Refactor the home page + createId comp to handle account

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import CreateId from '../components/Home/CreateId';
+import Dashboard from './Dashboard';
+import { useAccount } from 'wagmi';
 
 function Home() {
-  return (
-    <>
-      <CreateId />
-    </>
-  );
+  const { isConnected } = useAccount();
+
+  if (isConnected) {
+    return <Dashboard />;
+  }
+
+  return <CreateId />;
 }
 
 export default Home;
