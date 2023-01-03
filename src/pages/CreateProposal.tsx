46aff3c4a4d279ee42624fe5475b53cd8dfd1817
Quentin D.C <quent043@hotmail.com>
2023-01-03 12:03:48 +0000

    Added User creation or login for Job & Proposal creations.

--- src/pages/CreateProposal.tsx
+++ src/pages/CreateProposal.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Back from '../components/Back';
 import ProposalForm from '../components/Form/ProposalForm';
@@ -6,12 +6,26 @@ import Loading from '../components/Loading';
 import Steps from '../components/Steps';
 import TalentLayerContext from '../context/talentLayer';
 import useServiceById from '../hooks/useServiceById';
+import PushContext from '../messaging/context/pushUser';
+import { IUser } from '@pushprotocol/restapi';
 
 function CreateProposal() {
   const { account, user } = useContext(TalentLayerContext);
-
+  const { getOrCreateUser } = useContext(PushContext);
+  const [pushUser, setPushUser] = useState<IUser>();
   const { id } = useParams<{ id: string }>();
   const service = useServiceById(id || '1');
+
+  const handleGetOrCreatePushUser = async () => {
+    try {
+      if (getOrCreateUser && user?.address) {
+        const pushUser = await getOrCreateUser(user?.address);
+        setPushUser(pushUser);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   if (!service) {
     return <Loading />;
@@ -26,6 +40,22 @@ function CreateProposal() {
 
       <Steps targetTitle={'Filled the proposal form'} />
       {account?.isConnected && user && <ProposalForm user={user} service={service} />}
+      <Steps targetTitle={'Fill the proposal form'} />
+      {!pushUser && account?.isConnected && user && (
+        <div className='border border-gray-200 rounded-md p-8'>
+          <p className='text-gray-500 py-4'>
+            In order to create a proposal, you need to be registered to our decentralized messaging
+            service. Please sign in to our messaging service to verify your identity :
+          </p>
+          <button
+            type='submit'
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            onClick={() => handleGetOrCreatePushUser()}>
+            Connect to Push
+          </button>
+        </div>
+      )}
+      {account?.isConnected && user && pushUser && <ProposalForm service={service} />}
     </div>
   );
 }
