34144298a14fca175bc3985863ddfcd3c4c4a63a
Quentin D.C <quent043@hotmail.com>
2023-01-07 09:42:01 +0000

    Refactored getMessages logic in context
    Added throbber + error message on message send

--- src/components/ConnectBlock.tsx
+++ src/components/ConnectBlock.tsx
@@ -4,7 +4,8 @@ function ConnectBlock() {
   return (
     <div className='border border-gray-200 rounded-md p-8'>
       <p className='text-gray-500 py-4'>
-        In order to create your profile, post a job... you need first to connect your wallet
+        In order to create your profile, post a job, access messaging... you need first to connect
+        your wallet
       </p>
       <ConnectButton />
     </div>
--- src/messaging/components/MessageComposer.tsx
+++ src/messaging/components/MessageComposer.tsx
@@ -1,31 +1,46 @@
 import { Dispatch, SetStateAction } from 'react';
+import Loading from '../../components/Loading';
 
 interface IMessageComposerProps {
   messageContent: string;
   setMessageContent: Dispatch<SetStateAction<string>>;
   sendNewMessage: () => void;
+  sendingPending: boolean;
+  messageSendingErrorMsg: string;
 }
 
 const MessageComposer = ({
   setMessageContent,
   messageContent,
   sendNewMessage,
+  sendingPending,
+  messageSendingErrorMsg,
 }: IMessageComposerProps) => {
   return (
-    <div className='flex flex-row space-x-5 py-5'>
-      <input
-        className='w-full bg-gray-300 py-2 px-3 rounded-xl'
-        type='text'
-        onChange={e => setMessageContent(e.target.value)}
-        placeholder='Write a message'
-        value={messageContent}
-      />
-      <button
-        className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full'
-        onClick={sendNewMessage}>
-        Send
-      </button>
-    </div>
+    <>
+      <div className='flex flex-row space-x-5 py-5'>
+        <input
+          className='w-full bg-gray-300 py-2 px-3 rounded-xl'
+          type='text'
+          onChange={e => setMessageContent(e.target.value)}
+          placeholder='Write a message'
+          value={messageContent}
+        />
+        {sendingPending && <Loading />}
+        {!sendingPending && (
+          <button
+            className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full'
+            onClick={sendNewMessage}>
+            Send
+          </button>
+        )}
+      </div>
+      {messageSendingErrorMsg && (
+        <div>
+          <p className={'text-red-400 ml-1'}>{messageSendingErrorMsg}</p>
+        </div>
+      )}
+    </>
   );
 };
 
