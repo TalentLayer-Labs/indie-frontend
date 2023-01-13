f6b344105479dd2b6c3f52b1b4d3347435bc9e61
Quentin D.C <quent043@hotmail.com>
2023-01-13 11:01:08 +0000

    Changes to allow local setup.

--- src/chains.ts
+++ src/chains.ts
@@ -19,4 +19,21 @@ export const customChains = {
     },
     testnet: false,
   },
+  local: {
+    id: 1337,
+    name: 'localhost',
+    network: 'localhost',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Ethereum',
+      symbol: 'ETH',
+    },
+    rpcUrls: {
+      default: 'https://api.avax-test.network/ext/C/rpc',
+    },
+    blockExplorers: {
+      default: { name: 'testnet.snowTrace', url: 'https://testnet.snowtrace.io/' },
+    },
+    testnet: false,
+  };
 };
