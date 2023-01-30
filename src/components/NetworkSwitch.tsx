5766a70f1b03f4b7c5980100b3a7481381d88520
Akumaw <dev@akumaw.com>
2023-01-30 19:57:28 +0100

    Fix PR review

--- src/components/NetworkSwitch.tsx
+++ src/components/NetworkSwitch.tsx
@@ -13,7 +13,7 @@ const chainIdToName = (chainId: number) => {
     case 1337:
       return 'Localhost';
     case 80001:
-      return 'polygonMumbai';
+      return 'Mumbai';
     case 137:
       return 'Polygon';
     default:
@@ -48,12 +48,12 @@ function NetworkSwitch() {
         leaveTo='transform opacity-0 scale-95'>
         <Menu.Items className='absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
           <div className='py-1'>
-            <Menu.Item key={80001}>
-              <NetworkLink chaindId={80001} chainName='polygonMumbai' />
+            <Menu.Item key={5}>
+              <NetworkLink chaindId={5} chainName='Goerli' />
             </Menu.Item>
             {/* {import.meta.env.DEV && (
               <Menu.Item key={1337}>
-                <NetworkLink chaindId={1337} chainName='localhost' />
+                <NetworkLink chaindId={1337} chainName='Localhost' />
               </Menu.Item>
             )} */}
           </div>
