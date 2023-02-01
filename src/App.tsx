717683e63db6bc48c19d5cda9e199f7d24e12a35
Hutgin <dev@akumaw.com>
2023-02-01 11:02:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import { Web3Modal } from '@web3modal/react';
 
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 
-const chains = [chain.polygonMumbai];
+const chains = [chain.goerli];
 
 // Wagmi client
 const { provider } = configureChains(chains, [
