2051fc8c8b1b46c03a4ae6c80bcfac9381bac09d
Bertrand <bertrandrigal.pro@gmail.com>
2023-02-08 15:54:20 +0000

    Add search for users

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 import { getUsers } from '../queries/users';
 import { IUser } from '../types';
 
-const useUsers = (): { users: IUser[] } => {
+const useUsers = (searchQuery?: string): IUser[] => {
   const [users, setUsers] = useState<IUser[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getUsers(import.meta.env.VITE_PLATFORM_ID);
+        const response = await getUsers(import.meta.env.VITE_PLATFORM_ID, searchQuery);
         if (response?.data?.data?.users.length > 0) {
           setUsers(response.data.data.users);
         }
@@ -20,7 +20,7 @@ const useUsers = (): { users: IUser[] } => {
     fetchData();
   }, []);
 
-  return { users };
+  return users;
 };
 
 export default useUsers;
