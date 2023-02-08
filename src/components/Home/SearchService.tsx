a9a991c0ab4b1e6a88fe015ec837911317fe28b7
Bertrand <bertrandrigal.pro@gmail.com>
2023-02-08 15:57:47 +0000

    Clean up home page structure

---
+++ src/components/Home/SearchService.tsx
@@ -0,0 +1,21 @@
+import SearchServiceButton from '../Form/SearchServiceButton';
+
+function SearchService() {
+  return (
+    <div className='bg-gray-50'>
+      <div className='max-w-7xl mx-auto text-gray-900 px-4'>
+        <div className='flex justify-center items-center gap-10 flex-col py-20'>
+          <p className='text-5xl sm:text-7xl font-medium tracking-wider max-w-lg text-center'>
+            Find your <span className='text-indigo-600'>Next Job </span> Now
+          </p>
+          <p className='text-gray-500'>
+            Earn money doing what you love. Find a job that fits your skills and schedule.
+          </p>
+          <SearchServiceButton value={''} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SearchService;
