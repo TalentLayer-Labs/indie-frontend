15e420dd199b8d4de7dc7de58884a41cfc672096
Romain <romain@talentlayer.org>
2023-02-08 09:35:26 +0000

    set subgraph url at the config level

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
 import axios from 'axios';
+import { config } from '../config';
 
 export const processRequest = async (query: string): Promise<any> => {
   try {
-    return await axios.post(import.meta.env.VITE_SUBGRAPH_URL, { query });
+    return await axios.post(config.subgraphUrl, { query });
   } catch (err) {
     console.error(err);
     return null;
