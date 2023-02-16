3eb155f830135f7d18b6ec5ef6be75f5a9a359ed
Romain <romain@talentlayer.org>
2023-02-16 20:32:49 +0000

    optim fees queries

--- src/queries/fees.ts
+++ src/queries/fees.ts
@@ -1,26 +1,18 @@
 import { processRequest } from '../utils/graphql';
 
-export const getProtocolAndOriginServiceFee = (originServicePlatformId: number): Promise<any> => {
+export const getProtocolAndPlatformsFees = (
+  originServicePlatformId: number,
+  originValidatedProposalPlatformId: number,
+): Promise<any> => {
   const query = `
   {
     protocols {
       protocolEscrowFeeRate
     }
-    platforms(id:${originServicePlatformId}){
+    servicePlatform: platforms(id:${originServicePlatformId}){
       originServiceFeeRate
     }
-  }
-    `;
-
-  return processRequest(query);
-};
-
-export const getOriginProposalValidatedPlatformId = (
-  originProposalValidatedPlatformId: number,
-): Promise<any> => {
-  const query = `
-  {
-    platforms(id:${originProposalValidatedPlatformId}){
+    proposalPlatform: platforms(id:${originValidatedProposalPlatformId}){
       originValidatedProposalFeeRate
     }
   }
