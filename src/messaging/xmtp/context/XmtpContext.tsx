31988a39d381fc36948b70a1aef1d964b39b0975
Quentin D.C <quent043@hotmail.com>
2023-03-14 18:43:16 +0000

    Cleaned XMTP Context

--- src/messaging/xmtp/context/XmtpContext.tsx
+++ src/messaging/xmtp/context/XmtpContext.tsx
@@ -63,6 +63,7 @@ export const XmtpContextProvider = ({ children }: { children: ReactNode }) => {
   const initClient = async (wallet: Signer) => {
     console.log('initClient w signer: ', wallet);
     if (wallet && !providerState.client && signer) {
+      // eslint-disable-next-line no-useless-catch
       try {
         const keys = await Client.getKeys(signer, { env: 'dev' });
         const client = await Client.create(null, {
@@ -77,57 +78,28 @@ export const XmtpContextProvider = ({ children }: { children: ReactNode }) => {
           // getOneConversationMessages,
         });
       } catch (e: any) {
-        throw e;
-        /*TODO see if this is still useful... I think not, redefining the client
-         prevents from calling it again after rejecting metamask popup */
-        // setProviderState({
-        //   ...providerState,
-        //   client: undefined,
-        // });
+        console.log(e);
       }
     }
   };
 
   useEffect(() => {
-    console.log('xmtp on signer change', signer);
-    //Not disconnecting, did it manually in Messenging.tsx with Wagmi
-    // signer ? setProviderState({ ...providerState, initClient }) : disconnect();
-
-    console.log('Checking user existence');
     const checkUserExistence = async (): Promise<void> => {
       if (signer) {
         const userExists = await Client.canMessage(walletAddress as string, { env: 'dev' });
         setProviderState({ ...providerState, userExists, initClient });
       }
-      console.log('ProviderState: ', providerState);
-      console.log('InitClient: ', providerState.initClient);
     };
     checkUserExistence();
   }, [signer]);
-
-  // watchAccount(() => {
-  //   if (disconnect && initPush && user?.address) {
-  //     const changeUser = async () => {
-  //       disconnect();
-  //       //TODO not working: "The requested account and/or method has not been authorized by the user."
-  //       // await initPush(user?.address);
-  //       navigate(`/messaging`);
-  //     };
-  //     changeUser();
-  //   }
-  // });
 
   useEffect(() => {
     if (!providerState.client) return;
 
     const listConversations = async (): Promise<void> => {
-      // console.log('listConversations triggered by providerState.client: ', providerState.client);
       setProviderState({ ...providerState, loadingConversations: true, loadingMessages: true });
       const { client, conversationMessages, conversations } = providerState;
       if (client) {
-        // (await client.conversations.list()).forEach(conv =>
-        //   console.log(conv.context?.conversationId),
-        // );
         let conv: Conversation[] = [];
         try {
           conv = (await client.conversations.list()).filter(conversation =>
@@ -137,13 +109,9 @@ export const XmtpContextProvider = ({ children }: { children: ReactNode }) => {
           console.log('Error listing conversations - ', e);
         } finally {
           setProviderState({ ...providerState, loadingConversations: false });
-          console.log('conv loaded');
         }
 
-        // console.log('TLV2 conv', conv);
         Promise.all(
-          //TODO Here need to remove duplicates
-          // & Find latest conversation + load its messages
           conv.map(async conversation => {
             if (conversation.peerAddress !== walletAddress) {
               let messages: DecodedMessage[] = [];
@@ -155,12 +123,9 @@ export const XmtpContextProvider = ({ children }: { children: ReactNode }) => {
               }
               //Temp fix for conversation duplicates
               if (messages.length > 0) {
-                console.log('xmpt context - conversation', conversation);
-                // console.log('xmpt context - messages', messages);
                 const chatMessages: XmtpChatMessage[] = messages.map(message =>
                   buildChatMessage(message),
                 );
-                // console.log('xmpt context - chatMessages', chatMessages);
                 conversationMessages.set(conversation.peerAddress, chatMessages);
                 conversations.set(conversation.peerAddress, conversation);
               }
@@ -174,37 +139,11 @@ export const XmtpContextProvider = ({ children }: { children: ReactNode }) => {
           }),
         ).then(() => {
           setProviderState({ ...providerState, loadingMessages: false });
-          console.log('msgs loaded');
         });
       }
     };
     listConversations();
   }, [providerState.client]);
-
-  const getOneConversationMessages = async (conversation: Conversation): Promise<void> => {
-    if (conversation.peerAddress !== walletAddress) {
-      console.log('xmpt context - getting messages for conversation: ', conversation);
-      const { conversationMessages } = providerState;
-      let messages: DecodedMessage[] = [];
-      try{
-        // Returns a list of all messages to/from the peerAddress
-        messages = await conversation.messages();
-      } catch (e: any) {
-        console.log('Error listing messages - ', e);
-      }
-      //Temp fix for conversation duplicates
-      if (messages.length > 0) {
-        console.log('xmpt context - messages', messages);
-        const chatMessages: XmtpChatMessage[] = messages.map(message => buildChatMessage(message));
-        console.log('xmpt context - chatMessages', chatMessages);
-        conversationMessages.set(conversation.peerAddress, chatMessages);
-      }
-      setProviderState({
-        ...providerState,
-        conversationMessages,
-      });
-    }
-  };
 
   const value = useMemo(() => {
     return {
