cfd12101b6aa499f2c9d95f2d9537a26908fe9cc
Romain <romain@talentlayer.org>
2023-03-17 16:25:10 +0000

    fix empty value

--- src/components/Form/ProfileForm.tsx
+++ src/components/Form/ProfileForm.tsx
@@ -125,7 +125,7 @@ function ProfileForm() {
                 name='role'
                 className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
                 placeholder=''>
-                <option value='buyer'></option>
+                <option value=''></option>
                 <option value='buyer'>Buyer</option>
                 <option value='seller'>Seller</option>
                 <option value='buyer-seller'>Both</option>
