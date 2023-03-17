bfeba08288b432f786682e05d66eca7482cd0d69
Quentin D.C <quent043@hotmail.com>
2023-03-17 21:20:23 +0000

    Added new badges data

--- src/sismoGroupsData.ts
+++ src/sismoGroupsData.ts
@@ -8,7 +8,18 @@ export const TALENTLAYER_GROUPS: ISismoGroup[] = [
     name: 'Talentlayer Opensource Contributor',
     description: 'TalentLayer OpenSource Contributors',
     image: 'https://sismo-testnets-hub-static.s3.eu-west-1.amazonaws.com/badges/talentlayer.svg',
-    link: 'https://testnets.sismo.io/talentlayer',
+    link: 'https://testnets.sismo.io/?badge=talentlayer',
+  },
+  {
+    id: '0xd0e3be7641aa0f72257f0db02657fe82',
+    userInGroup: false,
+    specs:
+      'Collect all users that already work at least 1 time on a php project on TalentLayer protocol',
+    name: 'One PHP Job on TalentLayer',
+    description:
+      'Congratulations ! You did one PHP job on the TalentLayer protocol. Keep up the good work',
+    image: '/sismo-badges/sismo_1jobs_php.svg',
+    link: 'https://testnets.sismo.io/?badge=one-php-job-on-talentlayer',
   },
   {
     id: '0xf2e9a70ce2d0afe45d96c6c642042e8d',
@@ -16,29 +27,48 @@ export const TALENTLAYER_GROUPS: ISismoGroup[] = [
     specs: 'Collect all users from decentralized subgraph of the protocol',
     name: 'Talentlayer User',
     description: 'Be a user of talentLayer',
-    image: '/sismo-badges/sismo_5jobs5stars.svg',
-    link: 'https://testnets.sismo.io/talentlayer-users',
+    image: 'https://sismo-testnets-hub-static.s3.eu-west-1.amazonaws.com/badges/talentlayer.svg',
+    link: 'https://testnets.sismo.io/?badge=talentlayer-users',
   },
   {
-    id: '0x3d9da8a5c7fccfe2de7d6a4cc89af3ad',
+    id: '0x8eeacb260254de2ac6529c206c7a7cf2',
     userInGroup: false,
     specs:
-      'Collect all users that already work at least 1 times on a solidity project on TalentLayer protocol',
+      'Collect all users that already work at least 1 time on a solidity project on TalentLayer protocol',
     name: 'Solidity dev',
     description: 'Be skilled in solidity',
-    image:
-      'https://sismo-prod-factory-api-data.s3.eu-west-1.amazonaws.com/badge-request-store/badge.f625760e-697c-4289-8553-0b9cb563db77.svg',
-    link: 'https://testnets.sismo.io/talentlayer-solidity-1',
+    image: '/sismo-badges/sismo_1jobs_sol.svg',
+    link: 'https://testnets.sismo.io/?badge=talentlayer-solidity-1',
+  },
+  {
+    id: '0xda2adbb8da571d832ef0d6ba2e2d62e1',
+    userInGroup: false,
+    specs:
+      'Collect all users that completed work at least once for AAVE with a 5 rating on TalentLayer protocol',
+    name: 'Complete work with minimum 5 as rating for AAVE',
+    description: 'Completed work for AAVE with minimum 5 as rating',
+    image: '/sismo-badges/sismo_workedforaave.svg',
+    link: 'https://testnets.sismo.io/?badge=one-5-stars-job-completed-for-aave-on-talentlayer-protocol',
   },
   {
     id: '0x3d9da8a5c7fccfe2de7d6a4cc89af3ad',
     userInGroup: false,
     specs:
-      'Collect all users that completed work at least 1 times with a 5 rating on TalentLayer protocol',
-    name: '5 stars rating',
-    description: 'Complete work with minimum 5 as rating',
+      'Donated to the Gitcoin Grant Round 15 through bulkCheckout Contracts on Ethereum (0x7d655c57f71464B6f83811C55D84009Cd9f5221C), or on Polygon (0xb99080b9407436eBb2b8Fe56D45fFA47E9bb8877), or on ZKSync (0xde21f729137c5af1b01d73af1dc21effa2b8a0d6), or appear on the Gitcoin Grants Round 15 API',
+    name: '15th Round Gitcoin Grant Donor',
+    description: 'Donated in the 15th round of Gitcoin Grants',
     image:
-      'https://sismo-prod-factory-api-data.s3.eu-west-1.amazonaws.com/badge-request-store/badge.f16510da-1123-44b9-8c48-179d9e394cbb.svg',
-    link: 'https://testnets.sismo.io/talentlayer-rating5-1',
+      'https://sismo-prod-factory-api-data.s3.eu-west-1.amazonaws.com/badge-request-store/badge.683fa087-cc48-41a2-a30f-85f4b8498faa.svg',
+    link: 'https://testnets.sismo.io/?badge=did-work-for-gitcoin-dao',
+  },
+  {
+    id: '0x56539c4d90ce4ee7d4c21623b750abe1',
+    userInGroup: false,
+    specs:
+      'Collect all users that completed work at least once for Dogecoin with a 5 rating on TalentLayer protocol',
+    name: 'Complete work with minimum 5 as rating for DogeCoin',
+    description: 'Completed work for DogeCoin with minimum 5 as rating',
+    image: '/sismo-badges/sismo_workedfordoge.svg',
+    link: 'https://testnets.sismo.io/?badge=one-5-stars-job-completed-for-dogecoin-on-talentlayer-protocol',
   },
 ];
