1487bd43fbc34098b9883076d4e59c991646503a
Romain <romain@talentlayer.org>
2023-03-17 21:55:06 +0000

    fix align

--- src/components/SismoGroupCard.tsx
+++ src/components/SismoGroupCard.tsx
@@ -18,8 +18,8 @@ function SismoGroupCard({
   return (
     <div className='rounded-xl p-4 border border-gray-200'>
       <div className='flex flex-col items-top justify-between gap-4 w-full'>
-        <div className='flex flex-col justify-start items-start gap-4'>
-          <div className='flex items-center justify-start relative'>
+        <div className='flex flex-col justify-start items-start gap-4 relative'>
+          <div className='flex items-center justify-start'>
             <img src={sismoGroupData.image} className='w-10 mr-4' />
             <div className='flex flex-col'>
               <p className='text-gray-900 font-medium'>{sismoGroupData.name}</p>
