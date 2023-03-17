555b5f2d46e6ed1fb4e50adeb633a61a84a67d62
Romain <romain@talentlayer.org>
2023-03-17 19:20:45 +0000

    add TL groups

--- src/sismoGroupsData.ts
+++ src/sismoGroupsData.ts
@@ -7,8 +7,8 @@ export const TALENTLAYER_GROUPS: ISismoGroup[] = [
     specs: 'Help us building TalentLayer protocol',
     name: 'Talentlayer Opensource Contributor',
     description: 'TalentLayer OpenSource Contributors',
-    image: '/sismo-badges/sismo_talentofmonth_graphic.svg',
-    link: 'https://app.sismo.io/?badge=proof-of-attendance-zk-badge',
+    image: 'https://sismo-testnets-hub-static.s3.eu-west-1.amazonaws.com/badges/talentlayer.svg',
+    link: 'https://testnets.sismo.io/talentlayer',
   },
   {
     id: '0xf2e9a70ce2d0afe45d96c6c642042e8d',
@@ -17,16 +17,28 @@ export const TALENTLAYER_GROUPS: ISismoGroup[] = [
     name: 'Talentlayer User',
     description: 'Be a user of talentLayer',
     image: '/sismo-badges/sismo_5jobs5stars.svg',
-    link: 'https://app.sismo.io/?badge=proof-of-attendance-zk-badge',
+    link: 'https://testnets.sismo.io/talentlayer-users',
   },
   {
-    id: '0xb2f9ffd39037252f2be891b943bfcca4',
+    id: '0x3d9da8a5c7fccfe2de7d6a4cc89af3ad',
     userInGroup: false,
     specs:
-      'Attend EthCC4 (3695), or EthCC5 (53834), or Devcon VI (60695), or DevCon V (69), or DevConnect Co-work space (36029), or ETH New York Stacked Hacker (53425), or ETHBerlin 3 (65440), or Poap Sponsor boot @DappCon22 (63682) or met Patricio during events on December 2021 (15916) or on February 2022 (25149), or on March 2022 (30875), or on April 2022 (36528), or on May 2022 (42068), or on June 2022 (47144), or on July 2022 (53153), or on August 2022 (57318), or on September 2022 (63400)',
-    name: 'Holds a main event POAP',
-    description: 'Hold one of the POAPs from a curated list of events',
-    image: '/sismo-badges/sismo_500earned.svg',
-    link: 'https://app.sismo.io/?badge=proof-of-attendance-zk-badge',
+      'Collect all users that already work at least 1 times on a solidity project on TalentLayer protocol',
+    name: 'Solidity dev',
+    description: 'Be skilled in solidity',
+    image:
+      'https://sismo-prod-factory-api-data.s3.eu-west-1.amazonaws.com/badge-request-store/badge.f625760e-697c-4289-8553-0b9cb563db77.svg',
+    link: 'https://testnets.sismo.io/talentlayer-solidity-1',
+  },
+  {
+    id: '0x3d9da8a5c7fccfe2de7d6a4cc89af3ad',
+    userInGroup: false,
+    specs:
+      'Collect all users that completed work at least 1 times with a 5 rating on TalentLayer protocol',
+    name: '5 stars rating',
+    description: 'Complete work with minimum 5 as rating',
+    image:
+      'https://sismo-prod-factory-api-data.s3.eu-west-1.amazonaws.com/badge-request-store/badge.f16510da-1123-44b9-8c48-179d9e394cbb.svg',
+    link: 'https://testnets.sismo.io/talentlayer-rating5-1',
   },
 ];
