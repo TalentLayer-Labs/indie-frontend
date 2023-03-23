4da063912f519ab1a949d2ceb7f42146d96cc389
Romain <romain@talentlayer.org>
2023-03-23 10:37:19 +0000

    fix etherscan url for mumbai

--- src/components/MultiStepsTransactionToast.tsx
+++ src/components/MultiStepsTransactionToast.tsx
@@ -15,7 +15,7 @@ function MultiStepsTransactionToast({
       <a
         className='flex flex-col text-sm font-normal w-full pt-2'
         target='_blank'
-        href={`https://goerli.etherscan.io/tx/${transactionHash}`}>
+        href={`https://mumbai.polygonscan.com/tx/${transactionHash}`}>
         <span className='inline-flex full-w justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 '>
           Check on etherscan
         </span>
