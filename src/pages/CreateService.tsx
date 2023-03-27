b8dc6f064106018e27ddca3242659d50ecf40ca2
Romain <romain@talentlayer.org>
2023-03-27 15:33:36 +0100

    use new messaging context & connect button

--- src/pages/CreateService.tsx
+++ src/pages/CreateService.tsx
@@ -2,55 +2,12 @@ import { useContext } from 'react';
 import ServiceForm from '../components/Form/ServiceForm';
 import Steps from '../components/Steps';
 import TalentLayerContext from '../context/talentLayer';
-import PushContext from '../messaging/push/context/pushUser';
-import { createUserIfNecessary } from '@pushprotocol/restapi/src/lib/chat/helpers';
-import { XmtpContext } from '../messaging/xmtp/context/XmtpContext';
-import { useSigner } from 'wagmi';
+import ConnectButton from '../messaging/components/ConnectButton';
+import MessagingContext from '../messaging/context/messging';
 
 function CreateService() {
   const { account, user } = useContext(TalentLayerContext);
-  const { pushUser } = useContext(PushContext);
-  const { providerState } = useContext(XmtpContext);
-  const { data: signer } = useSigner({ chainId: import.meta.env.VITE_NETWORK_ID });
-
-  const userExists = (): boolean => {
-    if (import.meta.env.VITE_MESSENGING_TECH === 'push') {
-      if (pushUser) {
-        return !!pushUser;
-      }
-    } else if (import.meta.env.VITE_MESSENGING_TECH === 'xmtp') {
-      if (providerState) {
-        return providerState?.userExists;
-      }
-    }
-    return false;
-  };
-
-  const handleRegisterToMessaging = async (): Promise<void> => {
-    if (import.meta.env.VITE_MESSENGING_TECH === 'push') {
-      try {
-        if (user?.address) {
-          await createUserIfNecessary({ account: user.address });
-        }
-      } catch (e) {
-        console.error('CreateService - Error initializing Push client :', e);
-      }
-    }
-    if (import.meta.env.VITE_MESSENGING_TECH === 'xmtp') {
-      try {
-        if (user?.address && providerState?.initClient && signer) {
-          await providerState.initClient(signer);
-        }
-      } catch (e) {
-        console.error('CreateService - Error initializing XMTP client : ', e);
-      }
-    }
-  };
-
-  const renderConnectButton = () => {
-    if (import.meta.env.VITE_MESSENGING_TECH === 'xmtp') return 'Connect to XMTP';
-    else if (import.meta.env.VITE_MESSENGING_TECH === 'push') return 'Connect to Push';
-  };
+  const { userExists } = useContext(MessagingContext);
 
   return (
     <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
@@ -59,20 +16,17 @@ function CreateService() {
       </p>
 
       <Steps targetTitle={'Fill the job form'} />
+
       {!userExists() && account?.isConnected && user && (
         <div className='border border-gray-200 rounded-md p-8'>
           <p className='text-gray-500 py-4'>
             In order to create a service, you need to be registered to our decentralized messaging
             service Please sign in to our messaging service to verify your identity
           </p>
-          <button
-            type='submit'
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-            onClick={() => handleRegisterToMessaging()}>
-            {renderConnectButton()}
-          </button>
+          <ConnectButton />
         </div>
       )}
+
       {account?.isConnected && user && userExists() && <ServiceForm />}
     </div>
   );
