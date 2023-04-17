40a16bcbd97b6ee459569ccf265f89c547746162
Quentin D.C <quent043@hotmail.com>
2023-04-17 15:41:39 +0100

    Introduced pagination to service page
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -2,16 +2,28 @@ import ServiceItem from '../components/ServiceItem';
 import useServices from '../hooks/useServices';
 import { IService, ServiceStatusEnum } from '../types';
 import SearchServiceButton from '../components/Form/SearchServiceButton';
+import { useState } from 'react';
 
 function Services() {
+  const PAGE_SIZE = 3;
   const queryString = window.location.search;
   const searchQuery = new URLSearchParams(queryString).get('s') || undefined;
-  const services = useServices(
+  const [offset, setOffset] = useState(0);
+  const { noMoreData, services } = useServices(
     ServiceStatusEnum.Opened,
     undefined,
     undefined,
     searchQuery?.toLocaleLowerCase(),
+    PAGE_SIZE,
+    offset,
   );
+
+  //TODO add loader
+  //TODO marche pas avec search
+
+  const loadMore = () => {
+    setOffset(offset + PAGE_SIZE);
+  };
 
   return (
     <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
@@ -39,12 +51,23 @@ function Services() {
         })}
       </div>
 
-      {services.length === 20 && (
-        <a
-          href='#'
-          className='px-5 py-2  border border-indigo-600 rounded-full text-indigo-600 hover:text-white hover:bg-indigo-700'>
-          Load More
-        </a>
+      {services.length > 0 && !noMoreData && (
+        <div className='flex justify-center items-center gap-10 flex-col pb-5'>
+          <button
+            type='submit'
+            className={`px-5 py-2 mt-5 content-center border border-indigo-600 rounded-full text-indigo-600 
+              hover:text-white hover:bg-indigo-700
+            `}
+            disabled={noMoreData}
+            onClick={() => loadMore()}>
+            Load More
+          </button>
+        </div>
+      )}
+      {noMoreData && (
+        <div className='flex justify-center items-center gap-10 flex-col pb-5 mt-5'>
+          <p>No more Services...</p>
+        </div>
       )}
     </div>
   );
