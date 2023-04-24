37c2ab9f26028aa7d85e9c7008ea97f523179424
Quentin D.C <quent043@hotmail.com>
2023-04-24 14:55:11 +0100

    Added display condition in CreateOrEditProposal.tsx
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/CreateOrEditProposal.tsx
+++ src/pages/CreateOrEditProposal.tsx
@@ -9,6 +9,7 @@ import useServiceById from '../hooks/useServiceById';
 import ConnectButton from '../messaging/components/ConnectButton';
 import MessagingContext from '../messaging/context/messging';
 import useProposalById from '../hooks/useProposalById';
+import { ProposalStatusEnum, ServiceStatusEnum } from '../types';
 
 function CreateOrEditProposal() {
   const { account, user } = useContext(TalentLayerContext);
@@ -24,9 +25,15 @@ function CreateOrEditProposal() {
   return (
     <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
       <Back />
-      {existingProposal ? (
+      {existingProposal &&
+      existingProposal.status === ProposalStatusEnum.Pending &&
+      service.status === ServiceStatusEnum.Opened ? (
         <p className='text-5xl font-medium tracking-wider mb-8'>
           Edit <span className='text-indigo-600'>your proposal</span>
+        </p>
+      ) : service.status !== ServiceStatusEnum.Opened ? (
+        <p className='text-5xl font-medium tracking-wider mb-8'>
+          This service is not <span className='text-indigo-600'>open </span> for proposals any more
         </p>
       ) : (
         <p className='text-5xl font-medium tracking-wider mb-8'>
@@ -46,9 +53,12 @@ function CreateOrEditProposal() {
         </div>
       )}
 
-      {userExists() && account?.isConnected && user && (
-        <ProposalForm user={user} service={service} existingProposal={existingProposal} />
-      )}
+      {userExists() &&
+        account?.isConnected &&
+        user &&
+        service.status === ServiceStatusEnum.Opened && (
+          <ProposalForm user={user} service={service} existingProposal={existingProposal} />
+        )}
     </div>
   );
 }
