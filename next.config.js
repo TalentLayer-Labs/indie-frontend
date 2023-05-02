a14ebf2dffa7c36ce7cc197ef35e17562b0e2d5c
Romain <romain@talentlayer.org>
2023-05-02 20:43:37 +0200

    fix external ipfs image

--- next.config.js
+++ next.config.js
@@ -2,17 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['ipfs.io'],
+  },
+};
 
 module.exports = {
-  webpack: (config) => {
+  webpack: config => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: [{ loader: '@svgr/webpack', options: { icon: true } }],
-    })
+    });
 
-    return config
+    return config;
   },
   ...nextConfig,
-}
+};
