acf36046b69f1d2376a80bc84369a285e5a2c1bd
Quentin D.C <quent043@hotmail.com>
2023-05-05 08:13:09 +0100

    First draft on generateMetaEvidence method
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

---
+++ src/modules/Kleros/utils/types.ts
@@ -0,0 +1,22 @@
+export type MetaEvidence = {
+  fileURI: string;
+  fileHash: string;
+  fileTypeExtension: string;
+  category: string;
+  title: string;
+  description: string;
+  aliases: {
+    [string: string]: string;
+  };
+  question: string;
+  rulingOptions: {
+    type: string;
+    precision: number;
+    titles: string[];
+    descriptions: string[];
+  };
+  evidenceDisplayInterfaceURI: string;
+  evidenceDisplayInterfaceHash: string;
+  dynamicScriptURI: string;
+  dynamicScriptHash: string;
+};
