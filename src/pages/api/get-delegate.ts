c204b471a2de9253e84b441d82a8e0ade0b6d369
Akumaw <dev@akumaw.com>
2023-05-07 22:14:05 +0200

    wip

--- src/pages/api/get-delegate.ts
+++ src/pages/api/get-delegate.ts
@@ -24,7 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       TalentLayerID.abi,
       signer,
     );
-    const isDelegate = await talentLayerContract.isDelegate(59);
+    const isDelegate = await talentLayerContract.isDelegate(userId);
 
     if (isDelegate) {
       signer = Wallet.fromMnemonic(delegateSeedPhrase).connect(provider);
