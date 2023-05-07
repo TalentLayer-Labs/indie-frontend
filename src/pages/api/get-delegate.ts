e70f31203e136dc38382fe7f62db7880160b6aaa
Akumaw <dev@akumaw.com>
2023-05-07 17:50:15 +0200

    wip

--- src/pages/api/get-delegate.ts
+++ src/pages/api/get-delegate.ts
@@ -7,10 +7,7 @@ import TalentLayerID from '../../contracts/ABI/TalentLayerID.json';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const query = req.query;
   const { userId } = query;
-  let newSigner: ethers.Signer;
-
   const delegateSeedPhrase = process.env.NEXT_PRIVATE_SEED_PHRASE;
-
   const delegateWallet = Wallet.fromMnemonic(delegateSeedPhrase as string);
   const delegateProvider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL);
   const delegateSigner = delegateWallet.connect(delegateProvider);
