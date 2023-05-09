d3029472e57595c7a0aa81b86030c8118da325dd
Quentin D.C <quent043@hotmail.com>
2023-05-09 18:32:43 +0100

    Updates file dropper
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/modules/Kleros/components/FileDropper.tsx
+++ src/modules/Kleros/components/FileDropper.tsx
@@ -1,10 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
+import { useFormikContext } from 'formik';
 
 const FileDropper = ({
+  fileSelected,
   setFileSelected,
 }: {
+  fileSelected: File | undefined;
   setFileSelected: React.Dispatch<React.SetStateAction<File | undefined>>;
 }) => {
+  const formikProps = useFormikContext();
   const fileInputRef = useRef<any>();
 
   const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
@@ -23,6 +27,7 @@ const FileDropper = ({
     event.preventDefault();
     event.stopPropagation();
     const file = [...event.dataTransfer.files][0];
+    formikProps.setFieldValue('file', file);
 
     setFileSelected(file);
     // setFileSelected(event.dataTransfer?.files[0]);
@@ -30,30 +35,38 @@ const FileDropper = ({
     // callback(event.dataTransfer?.files[0]);
   };
 
-  return (
-    <div
-      className={`h-40 p-50 mt-1 flex items-center justify-center flex-col text-24 text-gray-600 border-2 border-dashed border-gray-300 rounded-lg`}
-      // onClick={() => fileInputRef?.current.click()}
-      onDrop={(e: React.DragEvent<HTMLDivElement>) => handleDrop(e)}
-      onDragOver={(e: React.DragEvent<HTMLDivElement>) => handleDragOver(e)}
-      onDragEnter={(e: React.DragEvent<HTMLDivElement>) => handleDragEnter(e)}
-      onDragLeave={(e: React.DragEvent<HTMLDivElement>) => handleDragLeave(e)}
-      ref={fileInputRef}>
-      {/*{fileSelected ? fileSelected.name : <StyledUploadIcon />}*/}
-      Drop stuff here
-    </div>
-    // <FileInput
-    //   ref={fileInputRef}
-    //   type="file"
-    //   onChange={(event) => {
-    //     //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    //     setFileSelected(event.target.files![0]);
-    //     //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    //     callback(event.target.files![0]);
-    //   }}
-    //   {...props}
-    // />
+  return !fileSelected ? (
+    <>
+      <span className='text-gray-700'>Drop your file here</span>
+      <div
+        className={`h-40 p-50 mt-1 flex items-center justify-center flex-col text-24 text-gray-600 border-2 border-dashed border-gray-300 rounded-lg`}
+        // onClick={() => fileInputRef?.current.click()}
+        onDrop={(e: React.DragEvent<HTMLDivElement>) => handleDrop(e)}
+        onDragOver={(e: React.DragEvent<HTMLDivElement>) => handleDragOver(e)}
+        onDragEnter={(e: React.DragEvent<HTMLDivElement>) => handleDragEnter(e)}
+        onDragLeave={(e: React.DragEvent<HTMLDivElement>) => handleDragLeave(e)}
+        ref={fileInputRef}>
+        {/*{fileSelected ? fileSelected.name : <StyledUploadIcon />}*/}
+        Drop stuff here
+      </div>
+    </>
+  ) : (
+    <>
+      <span className='text-gray-700'>Your file</span>
+      <div className={`mt-1 flex flex-col text-24 text-gray-600`}>{fileSelected.name}</div>
+    </>
   );
+  // <FileInput
+  //   ref={fileInputRef}
+  //   type="file"
+  //   onChange={(event) => {
+  //     //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  //     setFileSelected(event.target.files![0]);
+  //     //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  //     callback(event.target.files![0]);
+  //   }}
+  //   {...props}
+  // />
 };
 
 export default FileDropper;
