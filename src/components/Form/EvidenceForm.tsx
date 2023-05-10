1989315f62ee81cb928c3f4d22810d07f717d2ea
Quentin D.C <quent043@hotmail.com>
2023-05-10 10:52:43 +0100

    Reviewed logic of EvidenceForm.tsx
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/components/Form/EvidenceForm.tsx
+++ src/components/Form/EvidenceForm.tsx
@@ -1,10 +1,17 @@
-import { ErrorMessage, Field, Form, Formik, useFormikContext } from 'formik';
-import { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useContext, useState } from 'react';
 import * as Yup from 'yup';
 import SubmitButton from './SubmitButton';
 import FileDropper from '../../modules/Kleros/components/FileDropper';
-import { Evidence } from '../../modules/Kleros/utils/types';
+import { postToIPFS } from '../../utils/ipfs';
+import { ethers } from 'ethers';
+import { config } from '../../config';
+import { createMultiStepsTransactionToast, showErrorTransactionToast } from '../../utils/toast';
+import { generateEvidence } from '../../modules/Kleros/utils/generateMetaEvidence';
+import TalentLayerEscrow from '../../contracts/ABI/TalentLayerEscrow.json';
+import TalentLayerContext from '../../context/talentLayer';
+import { useWeb3Modal } from '@web3modal/react';
+import { useProvider, useSigner } from 'wagmi';
 
 interface IFormValues {
   title: string;
@@ -12,47 +19,77 @@ interface IFormValues {
   file: File | null;
 }
 
+const validationSchema = Yup.object({
+  title: Yup.string().required('Please provide a title for your evidence'),
+  about: Yup.string().required('Please provide a description of your evidence'),
+  file: Yup.mixed().required('Please provide a file'),
+});
+
 const initialValues: IFormValues = {
   title: '',
   about: '',
   file: null,
 };
 
-function EvidenceForm({
-  evidences,
-  setEvidences,
-}: {
-  evidences: Evidence[];
-  setEvidences: Dispatch<SetStateAction<Evidence[]>>;
-}) {
-  const [fileSelected, setFileSelected] = useState<File>();
-  // const [evidences, setEvidences] = useState<Evidence[]>([]);
-
-  const router = useRouter();
-
-  const validationSchema = Yup.object({
-    title: Yup.string().required('Please provide a title for your evidence'),
-    about: Yup.string().required('Please provide a description of your evidence'),
+function EvidenceForm({ transactionId }: { transactionId: string }) {
+  const { account, user } = useContext(TalentLayerContext);
+  const { open: openConnectModal } = useWeb3Modal();
+  const provider = useProvider({ chainId: parseInt(process.env.NEXT_PUBLIC_NETWORK_ID as string) });
+  const { data: signer } = useSigner({
+    chainId: parseInt(process.env.NEXT_PUBLIC_NETWORK_ID as string),
   });
+  const [fileSelected, setFileSelected] = useState<File>();
 
-  const onSubmit = async (values: IFormValues, { resetForm }: { resetForm: () => void }) => {
-    //TODO Upload file to ipfs
-    //TODO Get file extention
-    //TODO Build evidence
-    setEvidences([
-      ...evidences,
-      {
-        fileURI: '',
-        //TODO remove this
-        fileHash: values.file?.name || '',
-        fileTypeExtension: '',
-        name: values.title,
-        description: values.about,
-      },
-    ]);
-    // formikProps.setFieldValue('evidences', evidences);
-    resetForm();
-    setFileSelected(undefined);
+  const onSubmit = async (
+    values: IFormValues,
+    {
+      setSubmitting,
+      resetForm,
+    }: { setSubmitting: (isSubmitting: boolean) => void; resetForm: () => void },
+  ) => {
+    if (account?.isConnected === true && provider && signer) {
+      try {
+        const fileExtension = values.file?.name.split('.').pop();
+        console.log('fileExtension', fileExtension);
+        console.log('values.file', values.file);
+        const fileCid = await postToIPFS(JSON.stringify(values.file));
+        console.log('fileCid', fileCid);
+        const evidence = generateEvidence(
+          values.title,
+          values.about,
+          fileCid,
+          fileExtension as string,
+        );
+        console.log('evidence', evidence);
+        const evidenceCid = await postToIPFS(JSON.stringify(evidence));
+        console.log('evidenceCid', evidenceCid);
+
+        const contract = new ethers.Contract(
+          config.contracts.talentLayerEscrow,
+          TalentLayerEscrow.abi,
+          signer,
+        );
+        const tx = await contract.submitEvidence(user?.id, transactionId, evidenceCid);
+        await createMultiStepsTransactionToast(
+          {
+            pending: 'Submitting evidence...',
+            success: 'Congrats! Your evidence has been submitted',
+            error: 'An error occurred while submitting your evidence ',
+          },
+          provider,
+          tx,
+          'evidence',
+          evidenceCid,
+        );
+        setSubmitting(false);
+        resetForm();
+        setFileSelected(undefined);
+      } catch (error) {
+        showErrorTransactionToast(error);
+      }
+    } else {
+      openConnectModal();
+    }
   };
 
   return (
@@ -92,9 +129,12 @@ function EvidenceForm({
               <FileDropper setFileSelected={setFileSelected} fileSelected={fileSelected} />
 
               <Field type='hidden' id='file' name='file' />
+              <span className='text-red-500'>
+                <ErrorMessage name='file' />
+              </span>
             </label>
             <div className='flex flex-row justify-between items-center ml-4 sm:ml-2'>
-              <SubmitButton isSubmitting={isSubmitting} label='Add evidence' />
+              <SubmitButton isSubmitting={isSubmitting} label='Submit evidence' />
             </div>
           </div>
         </Form>
