02f7b83dc181093557d02ba4eb629b57b5200739
Quentin D.C <quent043@hotmail.com>
2023-05-10 20:32:40 +0100

    Transactions query
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

---
+++ src/queries/transactions.ts
@@ -0,0 +1,25 @@
+import { processRequest } from '../utils/graphql';
+
+export const getTransactionById = (id: string): Promise<any> => {
+  const query = `
+      {
+        transactions (where: {id: "${id}"}) {
+          arbitrationFeeTimeout
+          amount
+          disputeId
+          senderFee
+          receiverFee
+          evidences {
+            uri
+            party {
+              address
+              createdAt
+              handle
+              id
+            }
+          }
+        }
+      }
+    `;
+  return processRequest(query);
+};
