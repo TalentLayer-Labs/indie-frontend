2b2ca0480417991c2577721389dc5c14f1cd3c9f
Quentin D.C <quent043@hotmail.com>
2023-05-11 09:14:51 +0100

    Added draft of custom DisputeDetail.tsx component
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

---
+++ src/modules/Kleros/components/DisputeDetail.tsx
@@ -0,0 +1,9 @@
+function DisputeItem() {
+  return (
+    <div className={'flex flex-row gap-2 rounded-xl p-4 border border-gray-200'}>
+      <div className={''}></div>
+    </div>
+  );
+}
+
+export default DisputeItem;
