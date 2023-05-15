eea0db2140304d21438d36874d5cf4781ed7f01e
Akumaw <dev@akumaw.com>
2023-05-15 16:57:44 +0200

    re order handleDelegateActivation

--- src/pages/api/utils/getDelegationSigner.ts
+++ src/pages/api/utils/getDelegationSigner.ts
@@ -7,27 +7,27 @@ export async function handleDelegateActivation(
   userAddress: string,
   res: NextApiResponse,
 ): Promise<Wallet | null> {
-  if (process.env.NEXT_PUBLIC_ACTIVE_DELEGATE !== 'true') {
-    res.status(500).json('Delegation is not activated');
-    return null;
-  }
-
   const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_BACKEND_RPC_URL);
   const delegateSeedPhrase = process.env.NEXT_PRIVATE_DELEGATE_SEED_PHRASE;
   const getUser = await getUserByAddress(userAddress);
   const delegateAddresses = getUser.data?.data?.users[0].delegates;
+
+  if (process.env.NEXT_PUBLIC_ACTIVE_DELEGATE !== 'true') {
+    res.status(500).json('Delegation is not activated');
+    return null;
+  }
 
   if (!delegateSeedPhrase) {
     res.status(500).json('Delegate seed phrase is not set');
     return null;
   }
 
-  const signer = Wallet.fromMnemonic(delegateSeedPhrase).connect(provider);
-
   if (delegateAddresses.indexOf(config.delegation.address.toLowerCase()) === -1) {
     res.status(500).json('Delegation is not activated');
     return null;
   }
+
+  const signer = Wallet.fromMnemonic(delegateSeedPhrase).connect(provider);
 
   return signer;
 }
