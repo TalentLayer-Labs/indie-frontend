431d70c8adc706cdc661ccc2799e81fb697adbcf
Quentin D.C <quent043@hotmail.com>
2023-05-16 18:30:55 +0100

    Additional domains for displaying Lens posts
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['ipfs.io'],
+    domains: [
+      'ipfs.infura.io',
+      'media0.giphy.com',
+      'ipfs.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io',
+      'source.unsplash.com',
+      'arweave.net',
+      'images.lens.phaver.com',
+      'cdn.stamp.fyi',
+      '',
+    ],
   },
 };
 
