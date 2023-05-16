aba4df75f19278ecfd32381d845a8cf33d26dbd5
akugone <dev@akumaw.com>
2023-05-16 17:14:15 +0200

    wip

--- src/pages/api/delegate/mint-id.ts
+++ src/pages/api/delegate/mint-id.ts
@@ -35,6 +35,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(200).json({ transaction: transaction });
   } catch (error) {
     console.log('errorDebug', error);
-    res.status(500).json('tx failed');
+    res.status(500).json({ error: error });
   }
 }
