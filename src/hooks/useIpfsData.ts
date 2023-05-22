c1acf7ea73023382056f203e6e652df6b4dd6f96
Quentin D.C <quent043@hotmail.com>
2023-05-22 18:52:55 +0100

    Added Ipfs Hook
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

---
+++ src/hooks/useIpfsData.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import { readFromIpfs } from '../utils/ipfs';
+
+const useIpfsData = (cid: string): any | null => {
+  const [file, setFile] = useState<any | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!cid) {
+          return;
+        }
+        const response = await readFromIpfs(cid);
+        console.log('response', response);
+        if (response) {
+          setFile(response);
+        }
+      } catch (err: any) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, [cid]);
+
+  return file;
+};
+
+export default useIpfsData;
