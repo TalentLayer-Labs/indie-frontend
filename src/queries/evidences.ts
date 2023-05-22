34519e6e9f2c0ecad789fd133b559713f9e58a5f
Quentin D.C <quent043@hotmail.com>
2023-05-22 18:14:22 +0100

    Fixed error in graph evidence query
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/queries/evidences.ts
+++ src/queries/evidences.ts
@@ -1,17 +1,15 @@
 import { processRequest } from '../utils/graphql';
 
 export const getEvidencesByPartyAndTransaction = (transactionId: string): Promise<any> => {
-  let condition = `where: transaction_: {id: "${transactionId}"}}"`;
+  let condition = `where: {transaction_: {id: "${transactionId}"}`;
   condition += '}, orderBy: id, orderDirection: asc';
   const query = `
     {
       evidences(${condition}) {
         id
         uri
-        transaction
         party {
           id
-          handle
         }
       }
     }
