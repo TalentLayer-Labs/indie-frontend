93e3304350bf4621d10fba1b02c2a632aeafe9c3
Quentin D.C <quent043@hotmail.com>
2023-05-23 11:08:01 +0100

    WIP on dispute page
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/dispute/[id].tsx
+++ src/pages/dispute/[id].tsx
@@ -1,57 +1,61 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import TalentLayerContext from '../../context/talentLayer';
 import { useRouter } from 'next/router';
 import Loading from '../../components/Loading';
 import useProposalById from '../../hooks/useProposalById';
 import EvidenceForm from '../../components/Form/EvidenceForm';
-import { IEvidence, TransactionStatusEnum } from '../../types';
+import { TransactionStatusEnum } from '../../types';
 import { IERC1497Evidence } from '../../modules/Kleros/utils/types';
-import DisputeItem from '../../modules/Kleros/components/DisputeDetail';
 import useTransactionsById from '../../hooks/useTransactionsById';
 import TimeOutCountDown from '../../components/TimeoutCountDown';
 import DisputeButton from '../../components/DisputeButton';
 import useArbitrationCost from '../../hooks/useArbitrationCost';
 import { ethers } from 'ethers';
+import useEvidences from '../../hooks/useEvidences';
+import useIpfsJsonData from '../../hooks/useIpfsJsonData';
+import { config } from '../../config';
+import TalentLayerEscrow from '../../contracts/ABI/TalentLayerEscrow.json';
+import { useProvider, useSigner } from 'wagmi';
+import { toast } from 'react-toastify';
+import TransactionToast from '../../components/TransactionToast';
 
 function Dispute() {
+  const { data: signer } = useSigner({
+    chainId: parseInt(process.env.NEXT_PUBLIC_NETWORK_ID as string),
+  });
+  const provider = useProvider({ chainId: parseInt(process.env.NEXT_PUBLIC_NETWORK_ID as string) });
   const router = useRouter();
   const { id: proposalId } = router.query;
   const { account, user } = useContext(TalentLayerContext);
   const proposal = useProposalById(proposalId as string);
   const transactionId = proposal?.service?.transaction?.id;
   const transaction = useTransactionsById(transactionId as string);
-  // const arbitrationFee = useArbitrationCost(proposal?.service?.transaction?.arbitrator);
-  const arbitrationCost = useArbitrationCost('0x2CA01a0058cfB3cc4755a7773881ea88eCfBba7C');
-  console.log('arbitrationCost', arbitrationCost);
+  const arbitrationFee = useArbitrationCost(transaction?.arbitrator);
+  // const arbitrationCost = useArbitrationCost('0x2CA01a0058cfB3cc4755a7773881ea88eCfBba7C');
+  const evidences = useEvidences(transactionId);
+  // console.log('arbitrationCost', arbitrationFee);
+  // console.log('evidences', evidences);
 
-  // if (!transaction?.arbitrator || transaction?.arbitrator === ethers.constants.AddressZero) {
-  // }
+  const evidenceDetail: IERC1497Evidence = useIpfsJsonData(
+    evidences && evidences.length > 0 && evidences[0].uri,
+  );
+
+  if (
+    (transaction && !transaction?.arbitrator) ||
+    (transaction && transaction?.arbitrator === ethers.constants.AddressZero)
+  ) {
+    return;
+  }
+  console.log('transaction', transaction);
 
   const targetDate = 1683795557869 + Number(transaction?.arbitrationFeeTimeout) * 1000;
   // const targetDate = (transaction?.lastInteraction + transaction.arbitrationFeeTimeout) * 1000;
+  // console.log('targetDate', targetDate);
 
+  //TODO Multistep tx toast ? Or custom toast if no ipfs mapping on the graph ?
   //TODO Display both parties evidences
   //TODO Display link to meta evidences
-  //TODO The Graph, need the time where fee was paid, so far just got "last interaction"
-
-  // const evidences = useEvidences(transactionId);
-  // console.log('evidences', evidences);
-  const evidences: IEvidence[] = [
-    {
-      uri: 'QmQ2hcACF6r2Gf8PDxG4NcBdurzRUopwcaYQHNhSah6a8v',
-      party: {
-        id: '3',
-        address: '0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE',
-        handle: '0xRacoon',
-        rating: '0',
-        description: '',
-        userStats: '',
-      },
-      id: '',
-      transaction: '',
-      createdAt: '',
-    },
-  ];
+  //TODO Evidence Modal + Download evidence or display if pic ? Custom according to file extention ?
 
   const buyerEvidences = evidences.filter(
     evidence => evidence.party.id === proposal?.service.buyer.id,
@@ -68,15 +72,49 @@ function Dispute() {
     user.id !== proposal?.seller.id
   ) {
     router.back();
+    //TODO return; instead ?
   }
 
-  if (!proposal?.service?.transaction?.id) {
+  if (!transactionId) {
     return <Loading />;
   }
 
+  const raiseDispute = async () => {
+    if (signer) {
+      const contract = new ethers.Contract(
+        config.contracts.talentLayerEscrow,
+        TalentLayerEscrow.abi,
+        signer,
+      );
+      try {
+        const tx =
+          user?.id === transaction?.sender.id
+            ? await contract.payArbitrationFeeBySender(transactionId, { value: arbitrationFee })
+            : user?.id === transaction?.receiver.id
+            ? await contract.payArbitrationFeeByReceiver(transactionId, { value: arbitrationFee })
+            : '';
+        const receipt = await toast.promise(provider.waitForTransaction(tx.hash), {
+          pending: {
+            render() {
+              return <TransactionToast message={'Raising dispute...'} transactionHash={tx.hash} />;
+            },
+          },
+          success: 'A dispute has been raised',
+          error: 'An error occurred while raising the dispute',
+        });
+        if (receipt.status !== 1) {
+          throw new Error('Transaction failed');
+        }
+        router.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <>
-      {user && proposal?.service.transaction.status === TransactionStatusEnum.NoDispute && (
+      {user && (
         <>
           <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
             <p className='text-5xl font-medium tracking-wider mb-8'>
@@ -99,8 +137,11 @@ function Dispute() {
                       buyerEvidences.map(evidence => {
                         return (
                           <div className={'mb-4 pb-4'}>
-                            {/*TODO href to details ?*/}
-                            <div className={'mb-2'}>{evidence.uri}</div>
+                            <a
+                              href={`https://cloudflare-ipfs.com/ipfs/${evidence.uri}`}
+                              className={'mb-2'}>
+                              {evidence.uri}
+                            </a>
                           </div>
                         );
                       })}
@@ -110,17 +151,27 @@ function Dispute() {
                     {evidences.length > 0 &&
                       sellerEvidences.map(evidence => {
                         return (
-                          <div className={'mb-4 pb-4'}>
-                            {/*TODO href to details ?*/}
-                            <div className={'mb-2'}>{evidence.uri}</div>
+                          <div className={'flex flex-row'}>
+                            <a
+                              href={`https://cloudflare-ipfs.com/ipfs/${evidence.uri}`}
+                              className={'mb-1 hover:underline'}>
+                              {evidence.uri}
+                            </a>
+                            {/*<ArrowUpOnSquareIcon*/}
+                            {/*  className={'h-4 ml-2 cursor-pointer w-4 text-gray-900'}*/}
+                            {/*/>*/}
                           </div>
                         );
                       })}
                   </p>
                 </div>
-                <div className={'flex'}></div>
+                <div className={'flex'}>
+                  {evidenceDetail && (
+                    <img src={`https://cloudflare-ipfs.com/ipfs/${evidenceDetail.fileHash}`} />
+                  )}
+                </div>
 
-                <div className={'flex flex-row flex gap-2 border border-gray-200 rounded-xl p-4'}>
+                <div className={'flex flex-row h-min gap-2 border border-gray-200 rounded-xl p-4'}>
                   <div className={''}>
                     <p className={'text-sm text-gray-500 mt-2'}>
                       <strong>Status:</strong> {transaction?.status}
@@ -138,12 +189,14 @@ function Dispute() {
                     <>
                       <div className={'flex flex-col'}>
                         <div className={'flex flex-row'}>
-                          <TimeOutCountDown
-                            targetDate={
-                              targetDate
-                              // (transaction?.lastInteraction + transaction.arbitrationFeeTimeout) * 1000
-                            }
-                          />
+                          {transaction.status !== TransactionStatusEnum.NoDispute && (
+                            <TimeOutCountDown
+                              targetDate={
+                                targetDate
+                                // (transaction?.lastInteraction + transaction.arbitrationFeeTimeout) * 1000
+                              }
+                            />
+                          )}
                         </div>
                         <DisputeButton
                           isSender={user.id === transaction.sender.id}
@@ -159,13 +212,14 @@ function Dispute() {
                   )}
                 </div>
               </div>
-              {account?.isConnected && user && transactionId && (
-                <EvidenceForm transactionId={transactionId} />
+              {account?.isConnected && user && transactionId && signer && provider && (
+                <EvidenceForm transactionId={transactionId} signer={signer} provider={provider} />
               )}
               <div className={'flex w-full space-y-4 flex-raw pb-4'}>
                 <button
                   className={`ml-2 mt-4 px-5 py-2 border rounded-md hover:text-indigo-600 hover:bg-white border-indigo-600 bg-indigo-600 text-white bg-indigo-700'
-                }`}>
+                }`}
+                  onClick={() => raiseDispute()}>
                   Raise dispute
                 </button>
               </div>
