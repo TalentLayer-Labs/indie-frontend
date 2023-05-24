7c808a443d2006d3ac094c999f13687e9d8da5a6
Akumaw <dev@akumaw.com>
2023-05-24 11:29:06 +0200

    wip

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -64,8 +64,8 @@ function Services() {
       </div>
 
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
-        {/* We can swap services by filteredResult  */}
-        {services.map((service: IService, i: number) => {
+        {/* We can swap services by filteredServices  */}
+        {filteredServices.map((service: IService, i: number) => {
           return <ServiceItem service={service} key={i} />;
         })}
       </div>
