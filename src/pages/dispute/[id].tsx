4a6a49f8223f81260946295b3442b01c198f7df8
Quentin D.C <quent043@hotmail.com>
2023-05-24 18:49:06 +0100

    Added Steps component to Dispute page
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/pages/dispute/[id].tsx
+++ src/pages/dispute/[id].tsx
@@ -17,6 +17,7 @@ import { useProvider, useSigner } from 'wagmi';
 import { arbitrationFeeTimeout, payArbitrationFee } from '../../contracts/disputes';
 import MetaEvidenceModal from '../../modules/Kleros/components/MetaEvidenceModal';
 import { formatRateAmount } from '../../utils/web3';
+import Steps from '../../components/Steps';
 
 function Dispute() {
   const { data: signer } = useSigner({
@@ -38,6 +39,8 @@ function Dispute() {
   const evidenceDetail: IERC1497Evidence = useIpfsJsonData(
     evidences && evidences.length > 0 && evidences[1].uri,
   );
+  // transaction ? (transaction.status = TransactionStatusEnum.Resolved) : ';';
+  // transaction ? (transaction.ruling = 1) : ';';
 
   const isSender = (): boolean => {
     return !!user && !!transaction && user.id === transaction.sender.id;
@@ -81,7 +84,6 @@ function Dispute() {
 
   //TODO Multistep tx toast ? Or custom toast if no ipfs mapping on the graph ?
   //TODO Evidence Modal + Download evidence or display if pic ? Custom according to file extention ?
-  //TODO Add metaEvidence display link
 
   const buyerEvidences = evidences.filter(
     evidence => evidence.party.id === proposal?.service.buyer.id,
@@ -124,171 +126,171 @@ function Dispute() {
   }
 
   return (
-    <>
+    <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
+      <p className='text-5xl font-medium tracking-wider mb-8'>
+        Raise <span className='text-indigo-600'>a dispute</span>
+      </p>
+
+      <Steps targetTitle={'Access the dispute dashboard'} />
       {user && (
         <>
-          <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
-            <p className='text-5xl font-medium tracking-wider mb-8'>
-              Raise <span className='text-indigo-600'>a dispute</span>
-            </p>
-            <div className={'flex-col border border-gray-200 rounded-md p-8'}>
-              <h2 className='mb-2 text-gray-900 font-bold'>Dispute details:</h2>
-              {/*<DisputeItem />*/}
-              <div
-                className={
-                  'flex flex-row justify-between gap-2 rounded-xl p-4 border border-gray-200'
-                }>
-                <div className={'flex-col flex'}>
-                  <p className={'text-sm text-gray-500 mt-2'}>
-                    <strong>Service:</strong> {proposal.service.description?.title}
-                  </p>
-                  <p className={'text-sm text-gray-500 mt-2'}>
-                    <strong>Buyer evidences:</strong>
-                    {evidences.length > 0 &&
-                      buyerEvidences.map(evidence => {
-                        return (
-                          <div className={'mb-4 pb-4'} key={evidence.id}>
-                            <a
-                              href={`https://cloudflare-ipfs.com/ipfs/${evidence.uri}`}
-                              className={'mb-2'}>
-                              {evidence.uri}
-                            </a>
-                          </div>
-                        );
-                      })}
-                  </p>
-                  <p className={'text-sm text-gray-500 mt-2'}>
-                    <strong>Seller evidences:</strong>
-                    {evidences.length > 0 &&
-                      sellerEvidences.map(evidence => {
-                        return (
-                          <div className={'flex flex-row'} key={evidence.id}>
-                            <a
-                              href={`https://cloudflare-ipfs.com/ipfs/${evidence.uri}`}
-                              className={'mb-1 hover:underline'}>
-                              {evidence.uri}
-                            </a>
-                            {/*<ArrowUpOnSquareIcon*/}
-                            {/*  className={'h-4 ml-2 cursor-pointer w-4 text-gray-900'}*/}
-                            {/*/>*/}
-                          </div>
-                        );
-                      })}
-                  </p>
-                  {transaction &&
-                    transaction.receiver &&
-                    proposal &&
-                    proposal.service &&
-                    proposal.description && (
-                      <p className={'text-sm text-gray-500 mt-2'}>
-                        <strong>Meta evidence:</strong>
-                        <MetaEvidenceModal
-                          seller={transaction?.receiver}
-                          serviceData={proposal?.service}
-                          token={transaction.token}
-                          proposalData={{
-                            about: proposal?.description.about,
-                            expirationDate: proposal.expirationDate,
-                            rateAmount: proposal.rateAmount,
-                            rateToken: proposal.rateToken.address,
-                            videoUrl: proposal.description.video_url,
-                          }}
-                        />
-                      </p>
-                    )}
-                </div>
-                <div className={'flex'}>
-                  {/*{evidenceDetail && (*/}
-                  {/*  <img src={`https://cloudflare-ipfs.com/ipfs/${evidenceDetail.fileHash}`} />*/}
-                  {/*)}*/}
-                </div>
-
-                <div className={'flex flex-row h-min gap-2 border border-gray-200 rounded-xl p-4'}>
-                  <div className={''}>
-                    <p className={'text-sm text-gray-500 mt-2'}>
-                      <strong>Status:</strong> {transaction?.status}
-                    </p>
-                    <p className={'text-sm text-gray-500 mt-2'}>
-                      <strong>Arbitration fee:</strong>{' '}
-                      {arbitrationFee &&
-                        transaction?.token &&
-                        formatRateAmount(
-                          arbitrationFee.toString(),
-                          transaction?.token.address,
-                          transaction?.token.decimals,
-                        ).exactValue}{' '}
-                      MATIC
-                    </p>
-                    <div className={'text-sm text-gray-500 mt-2'}>
-                      <strong>Buyer fee:</strong>{' '}
-                      {transaction?.senderFeePaidAt ? (
-                        <span className='text-green-600 font-bold'>Paid</span>
-                      ) : (
-                        <span className='text-red-600 font-bold'>Not paid</span>
-                      )}
-                    </div>
-                    <p className={'text-sm text-gray-500 mt-2'}>
-                      <strong>Seller fee:</strong>{' '}
-                      {transaction?.receiverFeePaidAt ? (
-                        <span className='text-green-600 font-bold'>Paid</span>
-                      ) : (
-                        <span className='text-red-600 font-bold'>Not paid</span>
-                      )}
-                    </p>
-                  </div>
-                  {transaction && (
-                    <>
-                      <div className={'flex flex-col'}>
-                        <div className={'flex flex-row'}>
-                          {(transaction.status === TransactionStatusEnum.WaitingReceiver ||
-                            transaction.status === TransactionStatusEnum.WaitingSender) && (
-                            //TODO Remove subtraction
-                            <TimeOutCountDown targetDate={getTargetDate() - 777600000} />
-                          )}
+          <div className={'flex-col border border-gray-200 rounded-md p-8'}>
+            <h2 className='mb-2 text-gray-900 font-bold'>Dispute details:</h2>
+            {/*<DisputeItem />*/}
+            <div
+              className={
+                'flex flex-row justify-between gap-2 rounded-xl p-4 border border-gray-200'
+              }>
+              <div className={'flex-col flex'}>
+                <p className={'text-sm text-gray-500 mt-2'}>
+                  <strong>Service:</strong> {proposal.service.description?.title}
+                </p>
+                <p className={'text-sm text-gray-500 mt-2'}>
+                  <strong>Buyer evidences:</strong>
+                  {evidences.length > 0 &&
+                    buyerEvidences.map(evidence => {
+                      return (
+                        <div className={'mb-4 pb-4'} key={evidence.id}>
+                          <a
+                            href={`https://cloudflare-ipfs.com/ipfs/${evidence.uri}`}
+                            className={'mb-2 hover:underline'}>
+                            {evidence.uri}
+                          </a>
                         </div>
-                        <DisputeButton
-                          isSender={isSender()}
-                          isReceiver={isReceiver()}
-                          transactionStatus={transaction.status}
-                          disabled={getTargetDate() > Date.now()}
-                          payArbitrationFee={payFee}
-                          arbitrationFeeTimeout={timeout}
-                        />
-                        {transaction && transaction.ruling && (
-                          <span className='mt-4 ml-2 text-xl font-bold text-gray-600'>
-                            {transaction.ruling === 1 ? 'Sender Wins' : 'Receiver Wins'}
-                          </span>
+                      );
+                    })}
+                </p>
+                <p className={'text-sm text-gray-500 mt-2'}>
+                  <strong>Seller evidences:</strong>
+                  {evidences.length > 0 &&
+                    sellerEvidences.map(evidence => {
+                      return (
+                        <div className={'flex flex-row'} key={evidence.id}>
+                          <a
+                            href={`https://cloudflare-ipfs.com/ipfs/${evidence.uri}`}
+                            className={'mb-1 hover:underline'}>
+                            {evidence.uri}
+                          </a>
+                          {/*<ArrowUpOnSquareIcon*/}
+                          {/*  className={'h-4 ml-2 cursor-pointer w-4 text-gray-900'}*/}
+                          {/*/>*/}
+                        </div>
+                      );
+                    })}
+                </p>
+                {transaction &&
+                  transaction.receiver &&
+                  proposal &&
+                  proposal.service &&
+                  proposal.description && (
+                    <p className={'text-sm text-gray-500 mt-2'}>
+                      <strong>Meta evidence:</strong>
+                      <MetaEvidenceModal
+                        seller={transaction?.receiver}
+                        serviceData={proposal?.service}
+                        token={transaction.token}
+                        proposalData={{
+                          about: proposal?.description.about,
+                          expirationDate: proposal.expirationDate,
+                          rateAmount: proposal.rateAmount,
+                          rateToken: proposal.rateToken.address,
+                          videoUrl: proposal.description.video_url,
+                        }}
+                      />
+                    </p>
+                  )}
+              </div>
+              <div className={'flex'}>
+                {/*{evidenceDetail && (*/}
+                {/*  <img src={`https://cloudflare-ipfs.com/ipfs/${evidenceDetail.fileHash}`} />*/}
+                {/*)}*/}
+              </div>
+
+              <div className={'flex flex-row h-min gap-2 border border-gray-200 rounded-xl p-4'}>
+                <div className={''}>
+                  <p className={'text-sm text-gray-500 mt-2'}>
+                    <strong>Status:</strong> {transaction?.status}
+                  </p>
+                  <p className={'text-sm text-gray-500 mt-2'}>
+                    <strong>Arbitration fee:</strong>{' '}
+                    {arbitrationFee &&
+                      transaction?.token &&
+                      formatRateAmount(
+                        arbitrationFee.toString(),
+                        transaction?.token.address,
+                        transaction?.token.decimals,
+                      ).exactValue}{' '}
+                    MATIC
+                  </p>
+                  <div className={'text-sm text-gray-500 mt-2'}>
+                    <strong>Buyer fee:</strong>{' '}
+                    {transaction?.senderFeePaidAt ? (
+                      <span className='text-green-600 font-bold'>Paid</span>
+                    ) : (
+                      <span className='text-red-600 font-bold'>Not paid</span>
+                    )}
+                  </div>
+                  <p className={'text-sm text-gray-500 mt-2'}>
+                    <strong>Seller fee:</strong>{' '}
+                    {transaction?.receiverFeePaidAt ? (
+                      <span className='text-green-600 font-bold'>Paid</span>
+                    ) : (
+                      <span className='text-red-600 font-bold'>Not paid</span>
+                    )}
+                  </p>
+                </div>
+                {transaction && (
+                  <>
+                    <div className={'flex flex-col'}>
+                      <div className={'flex flex-row'}>
+                        {(transaction.status === TransactionStatusEnum.WaitingReceiver ||
+                          transaction.status === TransactionStatusEnum.WaitingSender) && (
+                          //TODO Remove subtraction
+                          <TimeOutCountDown targetDate={getTargetDate() - 777600000} />
                         )}
                       </div>
-                    </>
-                  )}
-                </div>
+                      <DisputeButton
+                        isSender={isSender()}
+                        isReceiver={isReceiver()}
+                        transactionStatus={transaction.status}
+                        disabled={getTargetDate() > Date.now()}
+                        payArbitrationFee={payFee}
+                        arbitrationFeeTimeout={timeout}
+                      />
+                      {transaction && transaction.ruling && (
+                        <span className='mt-4 ml-2 text-xl font-bold text-gray-600'>
+                          {transaction.ruling === 1 ? 'Sender Wins' : 'Receiver Wins'}
+                        </span>
+                      )}
+                    </div>
+                  </>
+                )}
               </div>
-              {account?.isConnected &&
-                user &&
-                transactionId &&
-                signer &&
-                provider &&
-                transaction?.status !== TransactionStatusEnum.Resolved && (
-                  <EvidenceForm transactionId={transactionId} signer={signer} provider={provider} />
-                )}
-              {transaction?.status === TransactionStatusEnum.NoDispute &&
-                signer &&
-                arbitrationFee && (
-                  <div className={'flex w-full space-y-4 flex-raw pb-4'}>
-                    <button
-                      className={`ml-2 mt-4 px-5 py-2 block hover:text-white rounded-lg text-center text-red-600 bg-red-50 hover:bg-red-500`}
-                      type='button'
-                      onClick={() => payFee()}>
-                      Raise dispute
-                    </button>
-                  </div>
-                )}
             </div>
+            {account?.isConnected &&
+              user &&
+              transactionId &&
+              signer &&
+              provider &&
+              transaction?.status !== TransactionStatusEnum.Resolved && (
+                <EvidenceForm transactionId={transactionId} signer={signer} provider={provider} />
+              )}
+            {transaction?.status === TransactionStatusEnum.NoDispute &&
+              signer &&
+              arbitrationFee && (
+                <div className={'flex w-full space-y-4 flex-raw pb-4'}>
+                  <button
+                    className={`ml-2 mt-4 px-5 py-2 block hover:text-white rounded-lg text-center text-red-600 bg-red-50 hover:bg-red-500`}
+                    type='button'
+                    onClick={() => payFee()}>
+                    Raise dispute
+                  </button>
+                </div>
+              )}
           </div>
         </>
       )}
-    </>
+    </div>
   );
 }
 
