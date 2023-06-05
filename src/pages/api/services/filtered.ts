3c2a0c066d140df4ab2b1f92ebfba07bde740df7
Akumaw <dev@akumaw.com>
2023-06-05 10:47:56 +0200

    Fix typing

--- src/pages/api/services/filtered.ts
+++ src/pages/api/services/filtered.ts
@@ -9,19 +9,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // @dev : here you can add the filter logic
   let keywordList = keywordFilter.keywords;
 
-  const getStringValue = (value: string | string[] | undefined): string | undefined => {
-    return typeof value === 'string' ? value : undefined;
-  };
-
-  const getNumberValue = (value: string | string[] | undefined): number | undefined => {
-    return value ? Number(value) : undefined;
-  };
-
-  const serviceStatus = getStringValue(query.serviceStatus) as ServiceStatusEnum;
-  const buyerId = getStringValue(query.buyerId);
-  const sellerId = getStringValue(query.sellerId);
-  const numberPerPage = getNumberValue(query.numberPerPage);
-  const offset = getNumberValue(query.offset);
+  const serviceStatus = query.serviceStatus as ServiceStatusEnum;
+  const buyerId = query.buyerId as string;
+  const sellerId = query.sellerId as string;
+  const numberPerPage = Number(query.numberPerPage);
+  const offset = Number(query.offset);
 
   try {
     let response = await getServices({
