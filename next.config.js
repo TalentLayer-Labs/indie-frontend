62d63780a648ebfa8b0952d5a569875be3e6f21f
Quentin D.C <quent043@hotmail.com>
2023-06-13 10:12:45 +0200

    Updated allowed domains (Made Lens module crash)
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- next.config.js
+++ next.config.js
@@ -21,3 +21,15 @@ const nextConfig = {
     ],
   },
 };
+
+module.exports = {
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
+  ...nextConfig,
+};
