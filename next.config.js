e83c85ab9b50e247f52b42b17718c2adfa787175
Quentin D.C <quent043@hotmail.com>
2023-06-13 09:59:03 +0200

    Added img domains to next config
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['ipfs.io'],
+    domains: [
+      'ipfs.infura.io',
+      'media0.giphy.com',
+      'ipfs.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io',
+      'source.unsplash.com',
+      'arweave.net',
+      'images.lens.phaver.com',
+      'cdn.stamp.fyi',
+      'ik.imagekit.io',
+      '',
+    ],
   },
-};
-
-module.exports = {
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
-    });
-
-    return config;
-  },
-  ...nextConfig,
 };
