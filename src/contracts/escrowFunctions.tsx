ff06f3e1168ddcd563010a96d0f9ad4c2b648494
Quentin D.C <quent043@hotmail.com>
2023-06-16 18:02:54 +0200

    Added claim balance function
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/contracts/escrowFunctions.tsx
+++ src/contracts/escrowFunctions.tsx
@@ -1,11 +1,15 @@
 import { Provider } from '@wagmi/core';
-import { BigNumber, Contract, Signer, ethers } from 'ethers';
+import { BigNumber, Contract, ethers, Signer } from 'ethers';
 import { toast } from 'react-toastify';
 import TransactionToast from '../components/TransactionToast';
 import { config } from '../config';
 import TalentLayerEscrow from './ABI/TalentLayerEscrow.json';
 import { showErrorTransactionToast } from '../utils/toast';
 import { delegateReleaseOrReimburse } from '../components/request';
+
+function getEscrowContract(signer: Signer) {
+  return new Contract(config.contracts.talentLayerEscrow, TalentLayerEscrow.abi, signer);
+}
 
 export const executePayment = async (
   userAddress: string,
@@ -30,11 +34,7 @@ export const executePayment = async (
       );
       tx = response.data.transaction;
     } else {
-      const talentLayerEscrow = new Contract(
-        config.contracts.talentLayerEscrow,
-        TalentLayerEscrow.abi,
-        signer,
-      );
+      const talentLayerEscrow = getEscrowContract(signer);
       tx = isBuyer
         ? await talentLayerEscrow.release(profileId, parseInt(transactionId, 10), amount.toString())
         : await talentLayerEscrow.reimburse(
@@ -56,6 +56,35 @@ export const executePayment = async (
       },
       success: isBuyer ? 'Payment release validated' : 'Payment reimbursement validated',
       error: 'An error occurred while validating your transaction',
+    });
+    if (receipt.status !== 1) {
+      throw new Error('Approve Transaction failed');
+    }
+  } catch (error: any) {
+    showErrorTransactionToast(error);
+  }
+};
+
+export const claimReferralBalance = async (
+  userId: string,
+  tokenAddress: string,
+  signer: Signer,
+  provider: Provider,
+): Promise<void> => {
+  try {
+    const talentLayerEscrow = getEscrowContract(signer);
+    const tx = await talentLayerEscrow.claimReferralBalance(userId, tokenAddress);
+
+    const message = 'Your claim is in progress';
+
+    const receipt = await toast.promise(provider.waitForTransaction(tx.hash), {
+      pending: {
+        render() {
+          return <TransactionToast message={message} transactionHash={tx.hash} />;
+        },
+      },
+      success: 'Claim validated',
+      error: 'An error occurred while processing your claim',
     });
     if (receipt.status !== 1) {
       throw new Error('Approve Transaction failed');
