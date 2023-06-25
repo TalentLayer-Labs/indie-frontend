aef815fc01ba13fa0f6cfbef7180e211b5316033
Namdar S <47576723+NamdarS@users.noreply.github.com>
2023-06-25 04:05:01 +0000

    back to local

--- src/components/UserOAuth.tsx
+++ src/components/UserOAuth.tsx
@@ -1,18 +1,31 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+
 export default function UserOAuth() {
   const { data: session } = useSession();
+  const buttonStyles = {
+    backgroundColor: "#FFC700",
+    color: "#1F2D3D",
+    fontWeight: "bold",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+    fontFamily: "'Helvetica Neue', sans-serif",
+  };
+
   if (session && session.user) {
     return (
       <>
         Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button style={buttonStyles} onClick={() => signOut()}>Sign out</button>
       </>
     );
   }
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button style={buttonStyles} onClick={() => signIn()}>Sign in</button>
     </>
   );
 }
+
