f60304a6e8185e884d33d244c2f1e985e5c3186f
Namdar S <47576723+NamdarS@users.noreply.github.com>
2023-06-25 03:14:41 +0000

    no prettier

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GitHubProvider from 'next-auth/providers/github';
 export default NextAuth({
     providers: [
         GitHubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
         })
     ],
 })
