7bc45a4912f36be55cc90f4cf6fd9b55f0a728de
Namdar S <47576723+NamdarS@users.noreply.github.com>
2023-06-25 05:50:08 +0000

    price cheaper

--- src/components/Form/handle-price.tsx
+++ src/components/Form/handle-price.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export function HandlePrice({ handle }: { handle: string }) {
   const length = handle.length;
-  const price = length > 4 ? 1 : 200 / Math.pow(2, handle.length - 1);
+  const price = 0.001;
   return (
     <div className='flex items-center border-gray-300 pl-2 text-sm text-gray-500 '>
       <div>{price} MATIC</div>
