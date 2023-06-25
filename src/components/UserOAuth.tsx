1f36fc277b758e57564fce9da8997ce5f2fba040
Namdar S <47576723+NamdarS@users.noreply.github.com>
2023-06-25 02:58:09 +0000

    undefined user warning fixed

--- src/components/UserOAuth.tsx
+++ src/components/UserOAuth.tsx
@@ -1,13 +1,13 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 export default function UserOAuth() {
   const { data: session } = useSession()
-  if (session) {
+  if (session && session.user) {
     return (
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    )
+    );
   }
   return (
     <>
