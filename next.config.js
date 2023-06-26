394da740d9d57c40977c9bebd9108c0f2c167de9
Quentin D.C <quent043@hotmail.com>
2023-06-26 16:13:53 +0200

    Update next.config.js
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- next.config.js
+++ next.config.js
@@ -1,24 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'ipfs.infura.io',
-      'media0.giphy.com',
-      'ipfs.io',
-      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
-      'lens.infura-ipfs.io',
-      'source.unsplash.com',
-      'arweave.net',
-      'images.lens.phaver.com',
-      'cdn.stamp.fyi',
-      'ik.imagekit.io',
-      '',
-    ],
+    domains: ['ipfs.io'],
   },
 };
 
@@ -29,6 +14,7 @@ module.exports = {
       issuer: /\.[jt]sx?$/,
       use: [{ loader: '@svgr/webpack', options: { icon: true } }],
     });
+
     return config;
   },
   ...nextConfig,
