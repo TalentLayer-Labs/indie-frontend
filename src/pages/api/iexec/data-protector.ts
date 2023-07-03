b36ef4d40211ff6fe2f58dc63a6f823a64a2b8ea
Akumaw <dev@akumaw.com>
2023-07-03 11:29:31 +0200

    add granted access

--- src/pages/api/iexec/data-protector.ts
+++ src/pages/api/iexec/data-protector.ts
@@ -1,6 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getIexecPrivateKey } from '../utils/iexec-private';
-import { IExecDataProtector, ProtectDataParams, getWeb3Provider } from '@iexec/dataprotector';
+import {
+  GrantAccessParams,
+  IExecDataProtector,
+  ProtectDataParams,
+  getWeb3Provider,
+} from '@iexec/dataprotector';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -12,12 +17,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const web3Provider = getWeb3Provider(privateKey);
     const dataProtector = new IExecDataProtector(web3Provider);
 
+    // We protect the data
     const protectDataArgs: ProtectDataParams = req.body;
     const protectedData = await dataProtector.protectData(protectDataArgs);
-
     console.log('Protected data:', protectedData);
 
-    res.status(200).json({ message: 'Data protected successfully', data: protectedData });
+    /* The address zero `0x0000000000000000000000000000000000000000` can be use to authorize any user to use the `protectedData` */
+    const authorizedUser = '0x0000000000000000000000000000000000000000';
+    const authorizedApp = process.env.NEXT_PUBLIC_MAIL_AUTHORIZE_APP_ADDRESS;
+
+    if (!authorizedApp) {
+      throw new Error('Authorized app is not set');
+    }
+
+    const grantAccessArgs: GrantAccessParams = {
+      protectedData: protectedData.address,
+      authorizedApp,
+      authorizedUser,
+    };
+
+    // We grant the access to the data
+    const grantedAccess = await dataProtector.grantAccess(grantAccessArgs);
+
+    res.status(200).json({
+      message: 'Data protected and access granted successfully',
+      data: { protectedData: protectedData, AccessGranted: grantedAccess },
+    });
   } catch (error) {
     console.log('errorDebug', error);
     res.status(500).json({ error: error });
