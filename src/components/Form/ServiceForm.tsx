9d08c2e60eed70ec44665626430567962d1cc6e4
Akumaw <dev@akumaw.com>
2023-07-06 11:37:53 +0200

    wip

--- src/components/Form/ServiceForm.tsx
+++ src/components/Form/ServiceForm.tsx
@@ -31,8 +31,7 @@ interface IFormValues {
 function ServiceForm({ serviceId }: { serviceId?: string }) {
   const { open: openConnectModal } = useWeb3Modal();
   const { user, account } = useContext(TalentLayerContext);
-  const { show, protectedMails, setShow, isRedirect } = useContext(Web3MailModalContext);
-  console.log('protectedMails', protectedMails);
+  const { setShow, isRedirect } = useContext(Web3MailModalContext);
 
   const provider = useProvider({ chainId: parseInt(process.env.NEXT_PUBLIC_NETWORK_ID as string) });
   const { data: signer } = useSigner({
@@ -167,9 +166,6 @@ function ServiceForm({ serviceId }: { serviceId?: string }) {
         );
         setSubmitting(false);
 
-        if (protectedMails.length === 0) {
-          setShow(true);
-        }
         resetForm();
         if (newId) {
           router.push(`/services/${newId}`);
--- src/context/web3email.tsx
+++ src/context/web3email.tsx
@@ -21,13 +21,9 @@ import { postToIPFS } from '../utils/ipfs';
 
 const Web3MailModalContext = createContext<{
   isRedirect: boolean;
-  protectedMails: any;
-  show: boolean;
   setShow: (show: boolean) => void;
 }>({
   isRedirect: true,
-  protectedMails: [],
-  show: false,
   setShow: () => {},
 });
 
@@ -38,6 +34,7 @@ const Web3MailModalProvider = ({ children }: { children: ReactNode }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
   const [proposalConsent, setProposalConsent] = useState(false);
   const { user } = useContext(TalentLayerContext);
+  console.log('user', user);
 
   const { isActiveDelegate } = useContext(TalentLayerContext);
   const [protectedMails, setProtectedMails] = useState([]);
@@ -148,8 +145,6 @@ const Web3MailModalProvider = ({ children }: { children: ReactNode }) => {
   const value = useMemo(() => {
     return {
       isRedirect,
-      protectedMails,
-      show,
       setShow,
     };
   }, [show, isRedirect]);
@@ -157,87 +152,88 @@ const Web3MailModalProvider = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Web3MailModalContext.Provider value={value}>{children}</Web3MailModalContext.Provider>
-
-      <div
-        className={`${
-          !show ? 'hidden' : ''
-        } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-full bg-black/75 flex flex-col items-center justify-center`}>
-        <div className='relative p-4 w-full max-w-2xl h-auto'>
-          <div className='relative bg-white rounded-lg shadow '>
-            <div className='flex justify-between items-start p-4 rounded-t border-b '>
-              <h3 className='text-xl font-semibold text-gray-900 '>Web3Mail information</h3>
-            </div>
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={onSubmit}>
-              {({ isSubmitting }) => (
-                <Form className='p-6 space-y-6'>
-                  <div className='flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6'>
-                    <div className='flex flex-row'>
-                      <h3 className='font-semibold text-gray-900'>
-                        Mail protection and grant access
-                      </h3>
-                    </div>
-                    <p>Here we will explain how the mail protector and grant access is working</p>
-                    <Field
-                      type='email'
-                      name='email'
-                      placeholder='Enter your email'
-                      className='...'
-                    />
-                    <ErrorMessage name='email' component='div' className='error-message' />
-                    <button
-                      type='submit'
-                      disabled={isSubmitting}
-                      className='block text-white bg-blue-600 font-medium rounded-lg px-5 py-2.5 text-center'>
-                      PROTECT MY MAIL
-                    </button>
-                    <div className='mt-4'>
-                      <div className='flex flex-row mb-4'>
-                        <h3 className='font-semibold text-gray-900'>Consent Management</h3>
+      {protectedMails.length === 0 && show && (
+        <div
+          className={`${
+            !show ? 'hidden' : ''
+          } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-full bg-black/75 flex flex-col items-center justify-center`}>
+          <div className='relative p-4 w-full max-w-2xl h-auto'>
+            <div className='relative bg-white rounded-lg shadow '>
+              <div className='flex justify-between items-start p-4 rounded-t border-b '>
+                <h3 className='text-xl font-semibold text-gray-900 '>Web3Mail information</h3>
+              </div>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}>
+                {({ isSubmitting }) => (
+                  <Form className='p-6 space-y-6'>
+                    <div className='flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6'>
+                      <div className='flex flex-row'>
+                        <h3 className='font-semibold text-gray-900'>
+                          Mail protection and grant access
+                        </h3>
                       </div>
-                      <p className='mb-4'>
-                        Here we will explain how the mail protector and grant access is working
-                      </p>
-                      <div className='flex flex-row mb-4'>
-                        <button
-                          onClick={() => setAccordionOpen(prevState => !prevState)}
-                          type='submit'
-                          disabled={isSubmitting}
-                          className='block text-black bg-white border border-black font-medium rounded-lg px-5 py-2.5 text-center mr-4'>
-                          LEARN MORE
-                        </button>
-                        <button
-                          onClick={async () => {
-                            await handleAgreeAndClose();
-                            setShow(false);
-                          }}
-                          type='submit'
-                          disabled={isSubmitting}
-                          className='block text-white bg-blue-600 font-medium rounded-lg px-5 py-2.5 text-center'>
-                          AGREE AND CLOSE
-                        </button>
-                      </div>
-                      {accordionOpen && (
-                        <div className='mt-4'>
-                          <label>
-                            <Toggle
-                              proposalConsent={proposalConsent}
-                              setProposalConsent={setProposalConsent}
-                            />
-                          </label>
+                      <p>Here we will explain how the mail protector and grant access is working</p>
+                      <Field
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email'
+                        className='...'
+                      />
+                      <ErrorMessage name='email' component='div' className='error-message' />
+                      <button
+                        type='submit'
+                        disabled={isSubmitting}
+                        className='block text-white bg-blue-600 font-medium rounded-lg px-5 py-2.5 text-center'>
+                        PROTECT MY MAIL
+                      </button>
+                      <div className='mt-4'>
+                        <div className='flex flex-row mb-4'>
+                          <h3 className='font-semibold text-gray-900'>Consent Management</h3>
                         </div>
-                      )}
+                        <p className='mb-4'>
+                          Here we will explain how the mail protector and grant access is working
+                        </p>
+                        <div className='flex flex-row mb-4'>
+                          <button
+                            onClick={() => setAccordionOpen(prevState => !prevState)}
+                            type='submit'
+                            disabled={isSubmitting}
+                            className='block text-black bg-white border border-black font-medium rounded-lg px-5 py-2.5 text-center mr-4'>
+                            LEARN MORE
+                          </button>
+                          <button
+                            onClick={async () => {
+                              await handleAgreeAndClose();
+                              setShow(false);
+                            }}
+                            type='submit'
+                            disabled={isSubmitting}
+                            className='block text-white bg-blue-600 font-medium rounded-lg px-5 py-2.5 text-center'>
+                            AGREE AND CLOSE
+                          </button>
+                        </div>
+                        {accordionOpen && (
+                          <div className='mt-4'>
+                            <label>
+                              <Toggle
+                                proposalConsent={proposalConsent}
+                                setProposalConsent={setProposalConsent}
+                              />
+                            </label>
+                          </div>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-            <div className='flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 '></div>
+                  </Form>
+                )}
+              </Formik>
+              <div className='flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 '></div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
