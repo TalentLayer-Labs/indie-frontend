dee81fac4aaf59ad5c76b21aa7dfad78eb32e14b
Romain <romain@talentlayer.org>
2023-07-26 11:37:02 +0200

    Fix webpack bundler issue with Next.JS

--- next.config.js
+++ next.config.js
@@ -4,16 +4,33 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['ipfs.io'],
-  }
+  },
 };
 
 module.exports = {
-  webpack: config => {
+  webpack: (config, { webpack }) => {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: [{ loader: '@svgr/webpack', options: { icon: true } }],
     });
+
+    config.plugins.push(
+      // polyfill 'node:buffer' and 'node:stream' used by file-type
+      new webpack.NormalModuleReplacementPlugin(/node:/, resource => {
+        const mod = resource.request.replace(/^node:/, '');
+        switch (mod) {
+          case 'buffer':
+            resource.request = 'buffer';
+            break;
+          case 'stream':
+            resource.request = 'readable-stream';
+            break;
+          default:
+            throw new Error(`Not found ${mod}`);
+        }
+      }),
+    );
 
     return config;
   },
